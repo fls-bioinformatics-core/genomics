d758422fec92a486aa7d3a532f2fad08ff34c926
Peter Briggs <peter.briggs@manchester.ac.uk>
2011-08-09 16:48:47 +0100

    run_qc_pipeline.py: use the "logging" module rather than print for info/debug etc.

--- NGS-general/SolidDataExtractor/run_qc_pipeline.py
+++ NGS-general/SolidDataExtractor/run_qc_pipeline.py
@@ -2,6 +2,7 @@ import sys
 import os
 import time
 import subprocess
+import logging
 
 def RunScript(script,csfasta,qual):
     # Runs a command
@@ -13,20 +14,20 @@ def RunScript(script,csfasta,qual):
 
 def QsubScript(name,script,*args):
     # Submits a command to the cluster
+    logging.info("Submitting job...")
     cmd_args = [script]
     cmd_args.extend(args)
     cmd = ' '.join(cmd_args)
     qsub=('qsub','-b','y','-cwd','-V','-N',name,cmd)
     cwd=os.path.dirname(csfasta)
     p = subprocess.Popen(qsub,cwd=cwd,stdout=subprocess.PIPE)
-    print "Submitting..."
     p.wait()
     # Capture the job id from the output
     job_id = None
     for line in p.stdout:
         if line.startswith('Your job'):
             job_id = line.split()[2]
-    print "Done - job id = %s" % job_id
+    logging.info("Done - job id = %s" % job_id)
     # Return the job id
     return job_id
 
@@ -64,6 +65,10 @@ if __name__ == "__main__":
         data_dir = sys.argv[2]
     print "Running %s on data from %s" % (script,data_dir)
 
+    # Set logging format and level
+    logging.basicConfig(format='%(levelname)8s %(message)s')
+    logging.getLogger().setLevel(logging.INFO)
+
     # Gather data files from data_dir
     all_files = os.listdir(data_dir)
     all_files.sort()
@@ -85,17 +90,17 @@ if __name__ == "__main__":
                 if os.path.exists(csfasta):
                     run_data.append((csfasta,qual))
             except IndexError:
-                print "Unable to process qual file %" % filen
+                logging.critical("Unable to process qual file %s" % filen)
             
     # For each pair of files, run the qc
     for data in run_data:
         # Check if queue is "full"
         while Qstat() >= 4:
             # Wait for 30 seconds
-            print "Waiting for free space in queue..."
+            logging.debug("Waiting for free space in queue...")
             time.sleep(30)
         # Submit
-        print "Submitting job: '%s %s %s'" % (script,data[0],data[1])
+        logging.info("Submitting job: '%s %s %s'" % (script,data[0],data[1]))
         QsubScript('qc',script,data[0],data[1])
         
     
