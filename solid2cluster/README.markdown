c8fbf92c229dfc099c3869c5066f92d7aa61189f
Peter Briggs <peter.briggs@manchester.ac.uk>
2011-08-17 17:10:02 +0100

    solid2cluster/rsync_solid_to_cluster.sh: initial version added to repository from local dev area.

---
+++ solid2cluster/README.markdown
@@ -0,0 +1,22 @@
+rsync_solid_to_cluster.sh
+=========================
+
+Interactive script to semi-automate the transfer of data from the SOLiD
+instrument to a destination machine.
+
+Usage:
+
+rsync_solid_to_cluster.sh <local_dir> <user>@<remote_host>:<remote_dir>
+
+<local_dir> is the directory to be copied; <user> is an account on the
+destination machine <remote_host> and <remote_dir> is the parent directory
+there where a copy of <local_dir> will be made.
+
+The script operates interactively and prompts the user at each step to
+confirm each action:
+
+1. Check that the information is correct
+2. Do "rsync --dry-run" and inspect the output
+3. Perform the actual rsync operation
+4. Check that the local and remote file sizes match
+5. Remove group write permissions from the remote copy
---
+++ solid2cluster/rsync_solid_to_cluster.sh
@@ -0,0 +1,268 @@
+#!/bin/sh
+#
+# rsync_solid_to_cluster.sh
+#
+# Script to do rsync of data from solid machine to cluster
+#
+# Usage: [ssh-agent] rsync_solid_to_cluster.sh <solid_run> <user>@<host>:<datadir>
+#
+# Note on using with ssh-agent:
+#
+# As this script performs a number of operations via ssh, it is possible
+# to invoke it via ssh-agent. If the script should then detect that the
+# ssh-agent is running and automatically invoke ssh-add, prompting for a
+# password/phrase - subsequent ssh operations will then not require
+# passwords to be supplied.
+#
+# This mode of operating with ssh-agent is preferred, as the ssh-agent
+# process terminates with the script (avoiding the proliferation of orphaned
+# ssh-agent processes on the local system).
+#
+# (Alternatively setting up unattended ssh login avoids the need for using
+# ssh-agent.)
+#
+#####################################################################
+# Script globals
+#####################################################################
+SCRIPT_NAME=`basename $0`
+usage="Usage: [ssh-agent] ${SCRIPT_NAME} <solid_run> <user>@<host>:<datadir>"
+NOW=$(date +"%Y%m%d%H%M")
+REMOTE_USER=
+REMOTE_HOST=
+REMOTE_DATADIR=
+RSYNC_EXCLUDES="--exclude=color* --exclude=cycleplots --exclude=jobs* --exclude=traffic_lights"
+RSYNC_OPTIONS="-av -e ssh $RSYNC_EXCLUDES"
+RSYNC_LOG="${SCRIPT_NAME%.*}_${NOW}.log"
+#
+#####################################################################
+# Functions
+#####################################################################
+#
+# prompt_user()
+#
+# Usage: prompt_user [ --exit <status> ] "my message" [ cmd ]
+#
+# Prompt the user to agree to an action: prints the supplied message
+# string with "[N/y]" appended, then waits for user input. Input of
+# the form "y", "Y", "Yes" or "yes" are confirmation, otherwise the
+# action is declined.
+#
+# If the action is confirmed then the functions returns 1. If an
+# optional command "cmd" was supplied to the function then this is also
+# executed.
+#
+# If the action is declined then the function returns 0, or if --exit
+# was supplied then the script exits with the supplied <status>.
+function prompt_user() {
+    # Check if --exit was supplied
+    exit_on_no=
+    if [ "$1" == "--exit" ] ; then
+	exit_on_no="true"
+	shift
+	exit_status=$1
+	shift
+    fi
+    # Check if a command was specified
+    cmd=
+    if [ ! -z "$2" ] ; then
+	cmd=$2
+    fi
+    # Prompt user for confirmation
+    echo -n "${1} [N/y]: "
+    read ok
+    case $ok in
+	y|Y|yes|Yes)
+	    # User agreed
+	    if [ ! -z "$cmd" ] ; then
+		# Execute the command
+		echo Executing $cmd
+		eval $cmd
+	    else
+		return 1
+	    fi
+	    ;;
+	*)
+	    # User declined
+	    if [ ! -z $exit_on_no ] ; then
+		# Exit the script
+		echo "Okay, stopped"
+		exit $exit_status
+	    else
+		if [ ! -z "$cmd" ] ; then
+		    # Command is not executed
+		    echo "Okay, operation skipped"
+		fi
+		return 0
+	    fi
+	    ;;
+    esac
+}
+#
+# acknowldge()
+#
+# Usage: acknowledge "my message"
+#
+# Prompt the user by printing the supplied message with the text
+# "(hit enter to continue)" appended, then wait for a newline before
+# passing control back to the calling subprogram.
+function acknowledge() {
+    echo -n "${1} (hit enter to continue)"
+    read ok
+    return
+}
+#
+# do_rsync()
+#
+# Usage: do_rsync [--dry-run] <solid_dir>
+#
+# Perform the requested rsync operation, using options taken from
+# the global shell variables. --dry-run invokes rsync with the dry run
+# option and pipes the output to less; otherwise the output is sent to
+# a log file.
+# Returns the exit status of the rsync command.
+function do_rsync() {
+    dry_run=
+    if [ "$1" == "--dry-run" ] ; then
+	dry_run="yes"
+	shift
+    fi
+    solid_run=$1
+    if [ ! -d "$solid_run" ] ; then
+	return
+    fi
+    opts=$RSYNC_OPTIONS
+    remote=${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_DATADIR}
+    if [ ! -z "$dry_run" ] ; then
+	# Dry run
+	acknowledge "Running rsync --dry-run: do 'q' to exit after reviewing"
+	opts="--dry-run $opts"
+	rsync_cmd="rsync $opts $solid_run $remote"
+	$rsync_cmd  | less
+	return $?
+    else
+	# Actual rsync
+	rsync_cmd="rsync $opts $solid_run $remote"
+	echo Executing $rsync_cmd
+	echo Writing output to $RSYNC_LOG
+	$rsync_cmd > $RSYNC_LOG 2>&1
+	# Return the output status of the rsync command
+	return $?
+    fi
+}
+#
+# compare_sizes()
+#
+# Usage: compare_sizes
+#
+# Get and compare the size of the source data on the local system with
+# that of the copy on the remote system.
+#
+# Return 1 if they're the same and 0 if they differ
+function compare_sizes() {
+    # Get size on local machine
+    local_size=`du --apparent-size -h $RSYNC_EXCLUDES -s $solid_run`
+    local_size=`echo $local_size | cut -f1 -d" "`
+    # Get size on remote system
+    du_cmd="du --apparent-size -h $RSYNC_EXCLUDES -s ${REMOTE_DATADIR}/$solid_run"
+    remote_size=`ssh ${REMOTE_USER}@${REMOTE_HOST} $du_cmd`
+    remote_size=`echo $remote_size | cut -f1 -d" "`
+    # Report
+    echo Local : $local_size
+    echo Remote: $remote_size
+    # Compare them
+    if [ "$local_size" != "$remote_size" ] ; then
+	return 0
+    else
+	return 1
+    fi
+}
+#
+#####################################################################
+# Main script
+#####################################################################
+#
+# Collect and process arguments
+solid_run=$1
+if [ ! -z "$2" ] ; then
+    REMOTE_USER=`echo $2 | cut -d@ -f1`
+    REMOTE_HOST=`echo $2 | cut -d@ -f2 | cut -d: -f1`
+    REMOTE_DATADIR=`echo $2 | cut -d@ -f2 | cut -d: -f2`
+fi
+#
+# Do checks
+if [ -z "$solid_run" ] ; then
+    echo $usage
+    exit 1
+elif [ ! -d "$solid_run" ] ; then
+    echo "${solid_run}: run not found"
+    exit 1
+fi
+if [ -z "$REMOTE_USER" ] || [ -z "$REMOTE_HOST" ] || [ -z "$REMOTE_DATADIR" ]
+then
+    echo Missing destination information
+    echo $usage
+    exit 1
+fi
+#
+# Report settings
+echo "Source:"
+echo "SOLiD directory : $solid_run"
+echo "Destination:"
+echo "Remote user     : $REMOTE_USER"
+echo "Remote host     : $REMOTE_HOST"
+echo "Remote directory: $REMOTE_DATADIR"
+#
+# Confirm that this is correct
+prompt_user --exit 0 "Proceed?"
+#
+# Check if ssh-agent is running
+if [ ! -z "$SSH_AUTH_SOCK" ] && [ ! -z "$SSH_AGENT_PID" ] ; then
+    echo "ssh-agent detected:"
+    echo SSH_AUTH_SOCK: $SSH_AUTH_SOCK
+    echo SSH_AGENT_PID: $SSH_AGENT_PID
+    echo "Invoking ssh-add:"
+    ssh-add
+fi
+#
+# Do dry run
+do_rsync --dry-run $solid_run
+#
+# Check that this is okay
+prompt_user --exit 0 "Proceed with rsync?"
+#
+# Do rsync
+echo "Starting rsync (this may take some time)"
+do_rsync $solid_run
+#
+# Check and report status
+status=$?
+echo -n "rsync completed: exit status = $status"
+if [ $status == 0 ] ;  then
+    echo " [OK]"
+else
+    echo " [ERROR]"
+    echo "*** WARNING: RSYNC RETURNED NON-ZERO EXIT STATUS ***"
+    echo "*** Check the rsync output in the log file       ***"
+    echo "*** Stopping ***"
+    exit 1
+fi
+#
+# Check sizes
+prompt_user "Check sizes of local and remote copies?" compare_sizes
+status=$?
+if [ $status != 1 ] ; then
+    echo "*** WARNING: SIZE OF LOCAL AND REMOTE COPIES DIFFERS ***"
+    echo "*** Check the remote copy and the rsync output       ***"
+    echo "*** Stopping ***"
+    exit 1
+else
+    echo "Local and remote sizes appear to match"
+fi
+#
+# Do chmod remotely
+ssh_chmod_cmd="ssh ${REMOTE_USER}@${REMOTE_HOST} 'chmod -R g-w ${REMOTE_DATADIR}/$solid_run'"
+prompt_user "Remove write permission from remote data?" "$ssh_chmod_cmd"
+echo "Done"
+exit 0
+##
+#
