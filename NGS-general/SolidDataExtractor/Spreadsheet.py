14e7d215c8ce30d350e709ad74817f96e8a49ef9
Peter Briggs <peter.briggs@manchester.ac.uk>
2011-08-22 13:11:38 +0100

    Spreadsheet.py: added new classes Workbook and Worksheet, which reimplement the spreadsheet writing functionality with a slightly modified interface intended to make it easier to create programmatic spreadsheet output.

--- NGS-general/SolidDataExtractor/Spreadsheet.py
+++ NGS-general/SolidDataExtractor/Spreadsheet.py
@@ -35,10 +35,233 @@ from xlwt.Utils import rowcol_to_cell
 from xlwt import easyxf
 
 import os
+import logging
 
 #######################################################################
 # Class definitions
 #######################################################################
+
+class Workbook:
+    """Class for writing data to an XLS spreadsheet.
+
+    A Workbook represents an XLS spreadsheet, which conists of sheets
+    (represented by Worksheet instances).
+    """
+
+    def __init__(self,xls_name=''):
+        """Create a new Workbook instance.
+
+        If the name of an existing XLS file is specified then the new
+        content will be appended to whatever is already in that
+        spreadsheet (note that the original spreadsheet will only be
+        overwritten if the same name is provided in the 'save' method).
+        Otherwise a new (empty) spreadsheet will be created.
+        """
+        self.name = xls_name
+        self.sheets = []
+        if not os.path.exists(self.name):
+            # New spreadsheet
+            self.workbook = xlwt.Workbook()
+            if self.name:
+                logging.warning("Specified XLS file '%s' not found" %
+                                self.name)
+        else:
+            # Spreadsheet already exists - convert into an xlwt workbook
+            rb = xlrd.open_workbook(self.name,formatting_info=True)
+            self.workbook = xlutils.copy.copy(rb)
+            # Collect the sheets in the workbook
+            i = 0
+            for s in rb.sheets():
+                logging.debug("Adding existing sheet: '%s'" % s.name)
+                sheet = self.addSheet(s.name,xlrd_sheet=s,xlrd_index=i)
+                i += 1
+
+    def addSheet(self,title,xlrd_sheet=None,xlrd_index=None):
+        """Add a new sheet to the spreadsheet.
+
+        Arguments:
+          title: title for the sheet
+          xlrd_sheet: (optional) an xlrd sheet from an existing XLS
+            workbook.
+        """
+        # Check if a sheet with this name already exists
+        logging.debug("Adding sheet '%s'" % title)
+        if xlrd_index is None and xlrd_sheet is None:
+            try:
+                ws = self.getSheet(title)
+                logging.warning("Sheet called '%s' already exists" % title)
+                return ws
+            except KeyError:
+                # Not found
+                ws = Worksheet(self.workbook,title)
+        else:
+            # Sheet already exists from original XLS
+            ws = Worksheet(self.workbook,title,xlrd_sheet=xlrd_sheet,
+                           xlrd_index=xlrd_index)
+        self.sheets.append(ws)
+        return ws
+
+    def getSheet(self,title):
+        """Retrieve a sheet from the spreadsheet.
+        """
+        for s in self.sheets:
+            logging.debug("Searching: sheet '%s'" % s.title)
+            if title == s.title: return s
+        raise KeyError, "No sheet called '%s' found" % title
+
+    def save(self,xls_name):
+        """Finish adding data and write the spreadsheet to disk.
+        """
+        # Write data for each sheet
+        for s in self.sheets:
+            s.save()
+        # Save workbook
+        if os.path.exists(xls_name):
+            logging.warning("Overwriting existing file: '%s'" % xls_name)
+        self.workbook.save(xls_name)
+
+class Worksheet:
+    """Class for writing to a sheet in an XLS spreadsheet.
+
+    A Worksheet object represents a sheet in an XLS spreadsheet.
+    """
+
+    def __init__(self,workbook,title,xlrd_index=None,xlrd_sheet=None):
+        """Create a new Worksheet instance.
+        """
+        self.title = title
+        self.workbook = workbook
+        if xlrd_index is None and xlrd_sheet is None:
+            # New worksheet
+            self.is_new = True
+            self.worksheet = self.workbook.add_sheet(title)
+            self.current_row = -1
+        else:
+            # Existing worksheet
+            self.is_new = False
+            self.worksheet = self.workbook.get_sheet(xlrd_index)
+            self.current_row = xlrd_sheet.nrows - 1
+        self.data = []
+
+    def addTabData(self,rows):
+        """Write a list of tab-delimited data rows to the sheet.
+
+        Given a list of rows with tab-separated data items,
+        write the data to the worksheet.
+
+        Arguments:
+          data: Python list representing rows of tab-separated
+            data items
+        """
+        for row in rows:
+            self.data.append(row)
+
+    def addText(self,text):
+        """Write text to the sheet.
+        """
+        return self.addTabData(text.split('\n'))
+
+    def insertColumn(self,position,insert_items=None,title=''):
+        """Insert a new column into the spreadsheet.
+        
+        This inserts a new column into each row of data, at the
+        specified positional index (starting from 0).
+
+        If insert_item starts with '=' then it's interpreted as a row-wise
+        formula. Formulas can be written in the form e.g. "=A+B-C", where
+        the letters indicate columns in the final XLS spreadsheet. When the
+        formulae are written they are expanded to include the row number
+        e.g. "=A1+B1-C1", "A2+B2-C2" etc.
+
+        Note: at present columns can only be inserted into new sheets.
+
+        Arguments:
+          position: positional index for the column to be inserted
+            at (0=A, 1=B etc)
+          title: (optional) value to be written to the first row (i.e. a column
+            title)
+          insert_items: value(s) to be inserted; either a single item, or a
+            list of items. Each item can be blank, a constant value, or a
+            formula.
+        """
+        if not self.is_new:
+            logging.error("ERROR cannot insert data into pre-existing worksheet")
+            return False
+        insert_title = True
+        # Loop over rows
+        for i in range(len(self.data)):
+            row = self.data[i]
+            items = row.split('\t')
+            new_items = []
+            for j in range(len(items)):
+                # Insert appropriate item at this position
+                if j == position:
+                    if insert_title:
+                        # Title for new column
+                        new_items.append(title)
+                        insert_title = False
+                    else:
+                        # Data item
+                        if isinstance(insert_items,list):
+                            try:
+                                insert_item = insert_items[j-1]
+                            except IndexError:
+                                # Ran out of items?
+                                insert_item = ''
+                        else:
+                            insert_item = insert_items
+                        new_items.append(insert_item)
+                # Append the existing row data
+                new_items.append(items[j])
+            # Replace old row with new one
+            self.data[i] = '\t'.join(new_items)
+        # Finished successfully
+        return True
+
+    def getColumnId(self,name):
+        """Lookup XLS column id from name of column.
+
+        If there is no data, or if the name isn't in the header
+        row of the data, then an exception is raised.
+
+        Returns the column identifier (i.e. 'A', 'B' etc) for the
+        column with the matching name.
+        """
+        try:
+            i = self.data[0].split('\t').index(name)
+            return string.uppercase[i]
+        except IndexError:
+            # Column name not found
+            raise IndexError, "Column '%s' not found" % name
+
+    def save(self):
+        """Write the new data to the spreadsheet.
+        """
+        for row in self.data:
+            self.current_row += 1
+            cindex = 0
+            for item in row.split('\t'):
+                if str(item).startswith('='):
+                    # Formula item
+                    formula = ''
+                    for c in item[1:]:
+                        formula += c
+                        if c.isalpha() and c.isupper:
+                            # Add the row number afterwards
+                            # NB xlwt takes row numbers from zero,
+                            # while XLS starts from 1
+                            formula += str(self.current_row+1)
+                    self.worksheet.write(self.current_row,cindex,
+                                         xlwt.Formula(formula))
+                else:
+                    # Data item
+                    self.worksheet.write(self.current_row,cindex,item)
+                cindex += 1
+        # Update/reset the sheet properties etc
+        self.data = []
+        self.is_new = False
+        # Finished
+        return
 
 class Spreadsheet:
     """Class for creating and writing a spreadsheet.
@@ -190,10 +413,22 @@ class Spreadsheet:
 #######################################################################
 
 if __name__ == "__main__":
-    # Example writing spreadsheet
+    # Example writing XLS with Spreadsheet class
     wb = Spreadsheet('test.xls','test')
     wb.addTitleRow(['File','Total reads','Unmapped reads'])
     wb.addEmptyRow()
     wb.addRow(['DR_1',875897,713425])
     wb.write()
+    # Example writing new XLS with Workbook class
+    wb = Workbook()
+    ws = wb.addSheet('test1')
+    ws.addText("Hello\tGoodbye\nGoodbye\tHello")
+    wb.save('test2.xls')
+    # Example appending to existing XLS with Workbook class
+    wb = Workbook('test2.xls')
+    ws = wb.getSheet('test1')
+    ws.addText("Some more data for you")
+    ws = wb.addSheet('test2')
+    ws.addText("Hahahah")
+    wb.save('test3.xls')
 
