3988152c891efcab7488348c7393f23f002338f8
Peter Briggs <peter.briggs@manchester.ac.uk>
2011-08-24 11:13:41 +0100

    mak_macs_xls.sh: updated to automatically set PYTHONPATH to find Spreadsheet module before running the Python code.

--- make_macs_xls.sh
+++ make_macs_xls.sh
@@ -5,32 +5,13 @@
 # Get the location for these scripts
 BIN_DIR=`dirname $0`
 #
-# See if Python virtualenv is available
-virtualenv=`which virtualenv`
-if [ "$virtualenv" == "" ] ; then
-    echo "virtualenv not found"
-    exit 1
+if [ -z "$PYTHONPATH" ] ; then
+    export PYTHONPATH=${BIN_DIR}/../share
+else
+    export PYTHONPATH=${PYTHONPATH}:${BIN_DIR}/../share
 fi
-#
-# Create and activate virtualenv for this run
-python_env=make_macs_xls_venv
-if [ ! -d $python_env ] ; then
-    virtualenv --no-site-packages $python_env
-fi
-if [ ! -e ${python_env}/bin/activate ] ; then
-    echo "unable to activate virtualenv"
-    exit 1
-fi
-. ${python_env}/bin/activate
-#
-# Get xlwt
-pip install xlwt
 #
 # Run the spreadsheet generator
 python ${BIN_DIR}/make_macs_xls.py $@
-#
-# Exit and destroy the virtualenv
-deactivate
-##rm -rf ${python_env}
-exit
 ##
+#
