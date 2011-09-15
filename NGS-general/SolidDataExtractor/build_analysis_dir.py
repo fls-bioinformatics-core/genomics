3f39d00ad0530ace8abf5d1070fa1a2c87c776f9
Peter Briggs <peter.briggs@manchester.ac.uk>
2011-09-15 14:32:15 +0100

    build_analysis_dir.py: substantial changes moving functionality out of __main__ and into existing Experiment class and new ExperimentList class; also added docstrings etc to clarify code function, intentions etc.

--- NGS-general/SolidDataExtractor/build_analysis_dir.py
+++ NGS-general/SolidDataExtractor/build_analysis_dir.py
@@ -24,8 +24,161 @@ import SolidDataExtractor
 # Class definitions
 #######################################################################
 
+class ExperimentList:
+    """Container for a collection of Experiments
+
+    Experiments are created and added to the ExperimentList by calling
+    the addExperiment method, which returns a new Experiment object.
+
+    The calling subprogram then populates the Experiment properties as
+    appropriate.
+
+    Once all Experiments are defined the analysis directory can be
+    constructed by calling the buildAnalysisDirs method, which creates
+    directories and symbolic links to primary data according to the
+    definition of each experiment.
+    """
+
+    def __init__(self,solid_run_dir=None):
+        """Create a new ExperimentList instance.
+
+        Arguments:
+          solid_run_dir: (optional) the path of the source SOLiD run
+            directory.
+        """
+        self.experiments = []
+        self.solid_run_dir = solid_run_dir
+        self.solid_runs = []
+        self.__getSolidRunData()
+
+    def __getSolidRunData(self):
+        """Get data about SOLiD runs
+
+        Internal function to construct SolidRun objects based on the
+        supplied SOLiD run directory.
+        """
+        if self.solid_run_dir is not None:
+            logging.debug("Acquiring run information")
+            for solid_dir in (self.solid_run_dir,self.solid_run_dir+"_2"):
+                logging.debug("Examining %s" % solid_dir)
+                run = SolidDataExtractor.SolidRun(solid_dir)
+                if not run:
+                    logging.debug("Unable to get run data for %s" % solid_dir)
+                else:
+                    self.solid_runs.append(run)
+            if len(self.solid_runs) == 0:
+                logging.warning("No run data found")
+
+    def addExperiment(self,name):
+        """Create a new Experiment and add to the list
+
+        Arguments:
+          name: the name of the new experiment
+
+        Returns:
+          New Experiment object with name already set
+        """
+        new_expt = Experiment()
+        new_expt.name = name
+        self.experiments.append(new_expt)
+        return new_expt
+
+    def addDuplicateExperiment(self,expt):
+        """Duplicate an existing Experiment and add to the list
+
+        Arguments:
+          expt: an existing Experiment object
+
+        Returns:
+          New Experiment object with the same data as the input
+        """
+        new_expt = expt.copy()
+        self.experiments.append(new_expt)
+        return new_expt
+
+    def getLastExperiment(self):
+        """Return the last Experiment added to the list
+        """
+        try:
+            return self.experiments[-1]
+        except IndexError:
+            return None
+
+    def buildAnalysisDirs(self,dry_run=False):
+        """Construct and populate analysis directories for the experiments
+
+        For each defined experiment, create the required analysis directories
+        and populate with links to the primary data files.
+
+        Arguments:
+          dry_run: if True then only report the mkdir, ln etc operations that
+            would be performed. Default is False (do perform the operations).
+        """
+        # For each experiment, make and populate directory
+        for expt in self.experiments:
+            expt_dir = expt.dirname()
+            logging.debug("Experiment dir: %s" % expt_dir)
+            # Make directory
+            if os.path.exists(expt_dir):
+                logging.warning("Directory %s already exists" % expt_dir)
+            else:
+                if not dry_run:
+                    # Create directory
+                    mkdir(expt_dir)
+                else:
+                    # Report what would have been done
+                    print "mkdir %s" % expt_dir
+            # Locate the primary data
+            for run in self.solid_runs:
+                libraries = run.fetchLibraries(expt.sample,expt.library)
+                for library in libraries:
+                    # Look up primary data
+                    ln_csfasta = getLinkName(library.csfasta,library)
+                    ln_qual = getLinkName(library.qual,library)
+                    logging.debug("Primary data:")
+                    logging.debug("\t%s" % ln_csfasta)
+                    logging.debug("\t%s" % ln_qual)
+                    # Make links to primary data
+                    self.__linkToFile(library.csfasta,os.path.join(expt_dir,ln_csfasta),dry_run=dry_run)
+                    self.__linkToFile(library.qual,os.path.join(expt_dir,ln_qual),dry_run=dry_run)
+    
+    def __linkToFile(self,source,target,dry_run=False):
+        """Create symbolic link to a file
+        
+        Internal function to make symbolic links to primary data. Checks that the
+        target links don't already exist, or if they do that the current source
+        file is the same as that specified in the method call.
+
+        Arguments:
+          source: the file to be linked to
+          target: the name of the link pointing to source
+          dry_run: if True then only report the actions that would be performed
+            (default is False, perform the actions)
+        """
+        # Check if target file already exists
+        if os.path.exists(target):
+            logging.warning("Target file %s already exists" % target)
+            # Test if the sources match
+            if os.readlink(target) != source:
+                logging.error("Different sources for %s" % target)
+            return
+        if not dry_run:
+            # Make symbolic links
+            mklink(source,target)
+        else:
+            # Report what would have been done
+            print "ln -s %s %s" % (source,target)
+
+    def __getitem__(self,key):
+        return self.experiments[key]
+
+    def __len__(self):
+        return len(self.experiments)
+
 class Experiment:
-    """Class to define an experiment from a SOLiD run.
+    """Class defining an experiment from a SOLiD run.
+
+    An 'experiment' is a collection of related data.
     """
     def __init__(self):
         """Create a new Experiment instance.
@@ -64,6 +217,16 @@ class Experiment:
         options.append("--source=%s/%s" % (sample,library))
         return ' '.join(options)
 
+    def copy(self):
+        """Return a new Experiment instance which is a copy of this one.
+        """
+        expt_copy = Experiment()
+        expt_copy.name = self.name
+        expt_copy.type = self.type
+        expt_copy.sample = self.sample
+        expt_copy.library = self.library
+        return expt_copy
+
 #######################################################################
 # Module functions
 #######################################################################
@@ -81,7 +244,7 @@ def match(pattern,word):
         # Match the whole word exactly
         return (word == pattern)
 
-def getLinkName(filen,sample,library):
+def getLinkName(filen,library):
     """Return the 'analysis' file name based on a source file name.
     
     The analysis file name is constructed as
@@ -94,13 +257,14 @@ def getLinkName(filen,sample,library):
     
     Arguments:
       filen: name of the source file
-      sample: SolidSample object representing the parent sample
-      library: SolidLibrary object representing the parent library
+      library: SolidLibrary object representing the parent library (nb
+         requires that the parent_sample of the library is set)
 
     Returns:
     Name for the analysis file.
     """
     # Construct new name
+    sample = library.parent_sample
     link_filen_elements = [sample.parent_run.run_info.instrument,
                            sample.parent_run.run_info.datestamp,
                            sample.name,library.name]
@@ -136,6 +300,7 @@ if __name__ == "__main__":
     print ""
     print "Options:"
     print "    --dry-run: report the operations that would be performed"
+    print "    --debug: turn on debugging output"
     print ""
     print "Defining experiments:"
     print ""
@@ -164,31 +329,32 @@ if __name__ == "__main__":
     print "was supplied then just the name is used) will be made, and links to"
     print "each of the primary data files."
 
-    # List of experiment definitions
-    expts = []
-    # Dry run flag
+    # Initialise
     dry_run = False
+
     # Process command line
     if len(sys.argv) < 2:
         # Insuffient arguments
         sys.exit(1)
-    solid_run_dir = None
-    for arg in sys.argv[1:]:
-        # Check
-        if solid_run_dir is not None:
-            # Still processing arguments after directory
-            # name was read
-            print "ERROR: additional argument after SOLiD run directory"
-            sys.exit(1)
+
+    # Solid run directory
+    solid_run_dir = sys.argv[-1]
+    if not os.path.isdir(solid_run_dir):
+        logging.error("Solid run directory '%s' not found" % solid_run_dir)
+        sys.exit(1)
+
+    # Set up experiment list
+    expts = ExperimentList(solid_run_dir=solid_run_dir)
+
+    # Process command line arguments
+    for arg in sys.argv[1:-1]:
         # Process command line arguments
         if arg.startswith('--name='):
-            expts.append(Experiment())
-            expt = expts[-1]
-            expt.name = arg.split('=')[1]
+            expt_name = arg.split('=')[1]
+            expt = expts.addExperiment(expt_name)
         elif arg.startswith('--type='):
-            try:
-                expt = expts[-1]
-            except IndexError:
+            expt = expts.getLastExperiment()
+            if expt is None:
                 print "No experiment defined for --type!"
                 sys.exit(1)
             if not expt.type:
@@ -197,18 +363,13 @@ if __name__ == "__main__":
                 print "Type already defined for experiment!"
                 sys.exit(1)
         elif arg.startswith('--source='):
-            try:
-                expt = expts[-1]
-            except IndexError:
+            expt = expts.getLastExperiment()
+            if expt is None:
                 print "No experiment defined for --source!"
                 sys.exit(1)
             if expt.sample:
                 # Duplicate the previous experiment
-                expts.append(Experiment())
-                expt_copy = expts[-1]
-                expt_copy.name = expt.name
-                expt_copy.type = expt.type
-                expt = expt_copy
+                expt = expts.addDuplicateExperiment(expt)
             # Extract sample and library
             source = arg.split('=')[1]
             try:
@@ -221,9 +382,6 @@ if __name__ == "__main__":
             dry_run = True
         elif arg == '--debug':
             logging.getLogger().setLevel(logging.DEBUG)
-        elif not arg.startswith('--'):
-            # Assume this is the last argument (source directory)
-            solid_run_dir = arg
         else:
             # Unrecognised argument
             print "Unrecognised argument: %s" % arg
@@ -233,8 +391,6 @@ if __name__ == "__main__":
     if not solid_run_dir:
         print "No SOLiD run directory specified, nothing to do"
         sys.exit(1)
-    if not os.path.isdir(solid_run_dir):
-        print "ERROR directory '%s' not found" % solid_run_dir
     if not len(expts):
         print "No experiments defined, nothing to do"
         sys.exit(1)
@@ -249,55 +405,11 @@ if __name__ == "__main__":
         print "\tOptions: %s" % expt.describe()
         print ""
 
-    # Get the run information
-    print "Acquiring run information:"
-    solid_runs = []
-    for solid_dir in (solid_run_dir,solid_run_dir+"_2"):
-        print "\t%s..." % solid_dir,
-        run = SolidDataExtractor.SolidRun(solid_dir)
-        if not run:
-            print "FAILED: unable to get run data for %s" % solid_dir
-        else:
-            solid_runs.append(run)
-            print "ok"
-    if not len(solid_runs):
+    # Check we have run data
+    if not len(expts.solid_runs):
         print "No run data found!"
         sys.exit(1)
 
-    # For each experiment, make a directory
-    for expt in expts:
-        expt_dir = expt.dirname()
-        print "\nExperiment: %s" % expt_dir
-        if not dry_run:
-            mkdir(expt_dir)
-        # Locate the primary data
-        for run in solid_runs:
-            for sample in run.samples:
-                if match(expt.sample,sample.name):
-                    # Found the sample
-                    for library in sample.libraries:
-                        if match(expt.library,library.name):
-                            # Found a matching library
-                            print "Located sample and library: %s/%s" % \
-                                (sample.name,library.name)
-                            # Look up primary data
-                            print "Primary data:"
-                            ln_csfasta = getLinkName(library.csfasta,
-                                                     sample,library)
-                            ln_qual = getLinkName(library.qual,
-                                                  sample,library)
-                            print "\t%s" % ln_csfasta
-                            print "\t%s" % ln_qual
-                            if not dry_run:
-                                # Make symbolic links
-                                mklink(library.csfasta,
-                                       os.path.join(expt_dir,ln_csfasta))
-                                mklink(library.qual,
-                                       os.path.join(expt_dir,ln_qual))
-                        else:
-                            # Library didn't match
-                            logging.debug("%s/%s: library didn't match pattern" %
-                                          (sample.name,library.name))
-                else:
-                    # Sample didn't match
-                    logging.debug("%s: ignoring sample" % sample.name)
+    # Build the analysis directory structure
+    expts.buildAnalysisDirs(dry_run=dry_run)
+            
