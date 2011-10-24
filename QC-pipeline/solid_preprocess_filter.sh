b1844faa5adeb106dcf798a460a7ce44c3cbab97
Peter Briggs <peter.briggs@manchester.ac.uk>
2011-10-24 10:51:09 +0100

    QC-pipeline: moved SOLiD_preprocess_filter step out of qc.sh into standalone solid_preprocess_filter.sh script.

---
+++ QC-pipeline/solid_preprocess_filter.sh
@@ -0,0 +1,122 @@
+#!/bin/sh
+#
+# Script to run fastq_screen QC steps on SOLiD data
+#
+# Usage: fastq_screen.sh <fastq>
+#
+function usage() {
+    echo "Usage: solid_process_filter.sh <csfasta> <qual>"
+    echo ""
+    echo "Run SOLID_preprocess with FLS Bioinf settings"
+    echo ""
+    echo "Input"
+    echo "  csfasta and qual file pair"
+    echo ""
+    echo "Output"
+    echo "  <csfasta_base>_T_F3.csfasta and <cfasta_base>_QV_T_F3.qual"
+}
+# Check command line
+if [ $# -ne 2 ] || [ "$1" == "-h" ] || [ "$1" == "--help" ] ; then
+    usage
+    exit
+fi
+#
+#===========================================================================
+# Import function libraries
+#===========================================================================
+#
+if [ -f functions.sh ] ; then
+    # Import local copies
+    . functions.sh
+else
+    # Import versions in share
+    . `dirname $0`/../share/functions.sh
+fi
+#
+#===========================================================================
+# Main script
+#===========================================================================
+#
+# Set umask to allow group read-write on all new files etc
+umask 0002
+#
+# Collect inputs
+csfasta=$(abs_path $1)
+qual=$(abs_path $2)
+#
+# Check files exist
+if [ ! -f "$csfasta" ] ; then
+    echo "ERROR csfasta file not found: $csfasta"
+    usage
+    exit 1
+fi
+if [ ! -f "$qual" ] ; then
+    echo "ERROR qual file not found: $qual"
+    usage
+    exit 1
+fi
+#
+# Set up environment
+QC_SETUP=`dirname $0`/qc.setup
+if [ -f "${QC_SETUP}" ] ; then
+    echo Sourcing qc.setup to set up environment
+    . ${QC_SETUP}
+else
+    echo WARNING qc.setup not found in `dirname $0`
+fi
+#
+# Set the programs
+# Override these defaults by setting them in qc.setup
+: ${SOLID_PREPROCESS_FILTER:=SOLiD_preprocess_filter_v2.pl}
+: ${FILTER_OPTIONS:="-x y -p 3 -q 22 -y y -e 10 -d 9"}
+#
+# Report
+echo ========================================================
+echo SOLiD_preprocess_filter
+echo ========================================================
+echo Started   : `date`
+echo Running in: `pwd`
+echo csfasta   : $csfasta
+echo qual      : $qual
+#
+# Output file names
+processed_csfasta=$(baserootname $csfasta)_T_F3.csfasta
+processed_qual=$(baserootname $csfasta)_QV_T_F3.qual
+#
+# Check if processed files already exist
+if [ -f "${processed_csfasta}" ] && [ -f "${processed_qual}" ] ; then
+    echo Filtered csfasta and qual files already exist, skipping preprocess filter
+else
+    # Make a temporary directory to run in
+    # This stops incomplete processing files being written to the working
+    # directory which might be left behind if the preprocessor stops (or
+    # is stopped) prematurely
+    wd=`pwd`
+    tmp=`mktemp -d`
+    cd $tmp
+    echo "Working in temporary directory ${tmp}"
+    # Run preprocessor
+    cmd="${SOLID_PREPROCESS_FILTER} -o $(baserootname $csfasta) ${FILTER_OPTIONS} -f ${csfasta} -g ${qual}"
+    echo $cmd
+    $cmd
+    # Move back to working dir and copy preprocessed files
+    cd $wd
+    if [ -f "${tmp}/${processed_csfasta}" ] ; then
+	/bin/cp ${tmp}/${processed_csfasta} .
+	echo Created ${processed_csfasta}
+    else
+	echo WARNING no file ${processed_csfasta}
+    fi
+    if [ -f "${tmp}/${processed_qual}" ] ; then
+	/bin/cp ${tmp}/${processed_qual} .
+	echo Created ${processed_qual}
+    else
+	echo WARNING no file ${processed_csfasta}
+    fi
+    # Remove temporary dir
+    /bin/rm -rf ${tmp}
+fi
+#
+echo solid_preprocess_filter completed: `date`
+exit
+#
