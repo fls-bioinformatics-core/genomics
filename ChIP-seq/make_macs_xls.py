c8c973dcf3b82fe07e706edf5fc445088ee28274
Peter Briggs <peter.briggs@manchester.ac.uk>
2011-11-16 16:27:27 +0000

    make_macs_xls.py: substantial rewriting to add several new columns, sort data, and add more explanatory text etc to additional sheets.

--- ChIP-seq/make_macs_xls.py
+++ ChIP-seq/make_macs_xls.py
@@ -24,6 +24,7 @@ SHARE_DIR = os.path.abspath(
     os.path.normpath(
         os.path.join(os.path.dirname(sys.argv[0]),'..','share')))
 sys.path.append(SHARE_DIR)
+from TabFile import TabFile
 import Spreadsheet
 
 #######################################################################
@@ -57,61 +58,97 @@ if __name__ == "__main__":
     print "Input file: %s" % macs_in
     print "Output XLS: %s" % xls_out
 
-    # Read in the MACS file and split into header and data
+    # Extract the header from the MACS and feed actual data to
+    # TabFile object
     header = []
-    data = []
+    data = TabFile(column_names=['chr','start','end','length','summit','tags',
+                                 '-10*log10(pvalue)','fold_enrichment','FDR(%)'])
     fp = open(macs_in,'r')
     for line in fp:
-        # Strip trailing newlines
-        line0 = line.strip('\n')
-        if line0.strip() == '':
-            # Skip empty lines
-            pass
-        elif line0.startswith('#'):
+        if line.startswith('#') or line.strip() == '':
             # Header line
-            header.append(line0)
+            header.append(line.strip())
         else:
-            # Data line
-            if not data:
-                # First line of data: prepend with '#'
-                line0 = '#'+line0
-            data.append(line0)
+            # Data
+            data.append(tabdata=line.strip())
     fp.close()
 
-    # Notes text example
-    # NB put tabs into the text to create new columns within a single line
-    notes = """Here is some example\t<style font=bold bgcolor=red>text</style>
+    # Temporarily remove first line
+    header_line = str(data[0])
+    del(data[0])
 
-Edit this as you wish and it will be added to the "notes" page
-of the spreadsheet, preserving the line breaks etc
+    # Sort into order by fold_enrichment and then by -10*log10(pvalue) column
+    data.sort(lambda line: line['fold_enrichment'],reverse=True)
+    data.sort(lambda line: line['-10*log10(pvalue)'],reverse=True)
 
-Hopefully it will work okay\t:)
+    # Restore first line
+    data.insert(0,tabdata=header_line)
+
+    # Insert "order" column
+    data.appendColumn("order")
+    data[0]['order'] = "order"
+    for i in range(1,len(data)):
+        data[i]['order'] = i
+    # Reorder columns to put it at the start
+    data = data.reorderColumns(['order','chr','start','end','length','summit','tags',
+                                '-10*log10(pvalue)','fold_enrichment','FDR(%)'])
+
+    # Legnds text
+    legends_text = """order\tSorting order Pvalue and FE
+chr\tChromosome location of binding region
+start\tStart coordinate of binding region
+end\tEnd coordinate of binding region
+summit-100\tSummit - 100bp
+summit+100\tSummit + 100bp
+summit-1\tSummit of binding region - 1
+summit\tSummit of binding region
+length\tLength of binding region
+summit\tPeak of summit region from the start position of the binding region
+tags\tNumber of non-degenerate and position corrected reads in the binding region
+-10*LOG10(pvalue)\tTransformed Pvalue -10*log10(Pvalue) for the binding region (e.g. if Pvalue=1e-10, then this value should be 100)
+fold_enrichment\tFold enrichment for this region against random Poisson distribution with local lambda
+FDR(%)\tFalse discovery rate (FDR) as a percentage
 """
     # Create a new spreadsheet
     wb = Spreadsheet.Workbook()
 
-    # Create sheets: "notes", "data" and "header"
-    ws_notes = wb.addSheet("Notes")
-    ws_data = wb.addSheet("Data")
-    ws_header = wb.addSheet("Header")
+    # Create the sheets
+    #
+    # data = the actual data from MACS
+    ws_data = wb.addSheet(os.path.basename(macs_in)[:min(30,len(os.path.basename(macs_in)))])
+    #
+    # note = the header data
+    ws_notes = wb.addSheet("notes")
+    ws_notes.addText("<style font=bold>MACS RUN NOTES:</style>")
+    ws_notes.addTabData(header)
+    ws_notes.addText("\n<style font=bold>ADDITIONAL NOTES:</style>\nBy default regions are sorted by Pvalue and fold enrichment (in descending order)")
+    #
+    # legends = static text explaining the column headers
+    ws_legends = wb.addSheet("legends")
+    ws_legends.addText(legends_text)
 
     # Add data to the "data" sheet
-    ws_data.addTabData(data)
+    ws_data.addText(str(data))
 
     # Insert formulae columns
     #
-    # Summit-100 = ("start"+"summit" columns) - 100
-    ws_data.insertColumn(3,title="summit-100",
-                         insert_items="=(B?+G?)-100")
-    # Summit+100 = ("start"+"summit" columns) + 100
-    ws_data.insertColumn(4,title="summit+100",
-                         insert_items="=(B?+G?)+100")
-    
-    # Add header to the "header" sheet
-    ws_header.addTabData(header)
-
-    # Add notes to the "notes" sheet
-    ws_notes.addText(notes)
+    # Copy of "chr" column
+    ws_data.insertColumn(4,title="chr",insert_items="=B?")
+    #
+    # Summit-100
+    ws_data.insertColumn(5,title="summit-100",insert_items="=J?-100")
+    #
+    # Summit+100
+    ws_data.insertColumn(6,title="summit+100",insert_items="=J?+100")
+    #
+    # Copy of "chr" column
+    ws_data.insertColumn(7,title="chr",insert_items="=B?")
+    #
+    # Summit-1
+    ws_data.insertColumn(8,title="summit-1",insert_items="=J?-1")
+    #
+    # Summit
+    ws_data.insertColumn(9,title="summit",insert_items="=C?+L?")
 
     # Write the spreadsheet to file
     wb.save(xls_out)
