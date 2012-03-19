28f7d6a7448cfd023d2fc8653750a6604c83eec1
Peter Briggs <peter.briggs@manchester.ac.uk>
2012-03-19 12:05:47 +0000

    analyse_solid_run.py: write full path to build_analysis_dir.py for --layout option.

--- solid2cluster/analyse_solid_run.py
+++ solid2cluster/analyse_solid_run.py
@@ -234,7 +234,8 @@ def suggest_analysis_layout(solid_runs):
     """
     print "#!/bin/sh\n#\n# Script commands to build analysis directory structure"
     for run in solid_runs:
-        build_analysis_dir_cmd = os.path.join(os.path.dirname(sys.argv[0]),'build_analysis_dir.py')
+        build_analysis_dir_cmd = os.path.abspath(os.path.join(os.path.dirname(sys.argv[0]),
+                                                              'build_analysis_dir.py'))
         cmd_line = [ build_analysis_dir_cmd,
                      "--top-dir=%s_analysis" % run.run_dir,
                      "--link=relative" ]
