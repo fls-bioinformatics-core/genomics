9fb9b6fdbb22b445af874d4be5c1addfe2a25925
Peter Briggs <peter.briggs@manchester.ac.uk>
2012-03-20 13:58:29 +0000

    extract_random.py: add -m option to extract records by regular expression matching.

--- utils/extract_random.py
+++ utils/extract_random.py
@@ -22,7 +22,7 @@ each file.
 # Module metadata
 #######################################################################
 
-__version__ = "0.0.1"
+__version__ = "0.1.0"
 
 #######################################################################
 # Import modules
@@ -32,6 +32,7 @@ import sys,os
 import random
 import logging
 import optparse
+import re
 
 #######################################################################
 # Classes
@@ -49,6 +50,7 @@ class ReadExtractor:
         """
         # Initialise
         self.__file_name = file_name
+        self.__file_type = None
         if lines_per_record is not None:
             self.__lines_per_record = lines_per_record
         else:
@@ -69,13 +71,13 @@ class ReadExtractor:
     def __set_lines_per_record(self):
         """Internal: set number of lines per record based on file extension
         """
-        file_type = os.path.splitext(self.__file_name)[1].strip('.')
-        if file_type == "csfasta" or file_type == "qual":
+        self.__file_type = os.path.splitext(self.__file_name)[1].strip('.')
+        if self.__file_type == "csfasta" or self.__file_type == "qual":
             self.__lines_per_record = 2
-        elif file_type == "fastq":
+        elif self.__file_type == "fastq" or self.__file_type == "fq":
             self.__lines_per_record = 4
         else:
-            logging.warning("Unknown file type: '%s'" % file_type)
+            logging.warning("Unknown file type: '%s'" % self.__file_type)
             self.__lines_per_record = 1
 
     @property
@@ -114,6 +116,28 @@ class ReadExtractor:
             current_line += 1 
         fp.close()
 
+    def regex_subset(self,regex_pattern):
+        """Extract and write subset of records matching a regular expression
+
+        Arguments:
+          regex_pattern: Python-style regular expression
+        """
+        fr = open(self.__file_name,'rU')
+        fp = open(os.path.basename(self.__file_name)+'.subset','w')
+        regex = re.compile(regex_pattern)
+        print "Outputting to %s" % os.path.basename(self.__file_name)+'.subset'
+        record = []
+        for line in fr:
+            record.append(line)
+            if len(record) == self.__lines_per_record:
+                # See if the record matches the pattern
+                if regex.search(''.join(record)):
+                    for line0 in record:
+                        fp.write(line0)
+                # Reset for next record
+                record = []
+        fp.close()
+
 #######################################################################
 # Functions
 #######################################################################
@@ -128,12 +152,16 @@ if __name__ == "__main__":
     # Process the command line
     p = optparse.OptionParser(usage="%prog OPTIONS infile [infile ...]",
                               version="%prog "+__version__,
-                              description="Extract a random subset of reads from each of the "
-                              "supplied files - where multiple files are specified, the same "
-                              "subsets will be extracted for all of them. Output file names "
+                              description="Extract subsets of reads from each of the "
+                              "supplied files according to specified criteria (e.g. "
+                              "random, matching a pattern etc). Output file names "
                               "are the input file names with '.subset' appended.")
+    p.add_option('-m','--match',action='store',dest='pattern',default=None,
+                 help="Extract records that match regular expression PATTERN")
     p.add_option('-n',action='store',dest='N',default=500,type="int",
-                 help="Number of records to extract from the input file(s) (default 500)")
+                 help="Extract N random records from the input file(s) (default 500). If "
+                 "multiple files are specified, the same subsets will be extracted for "
+                 "each.")
     options,arguments = p.parse_args()
     if len(arguments) < 1:
         p.error("No input files supplied")
@@ -142,25 +170,34 @@ if __name__ == "__main__":
     p.print_version()
 
     # Initialise
-    N = options.N
-    print "Extracting %d unique random reads from input files" % N
-    # Loop over input files and collect data
-    nrecords = None
-    for f in arguments:
-        if nrecords is None:
-            nrecords = ReadExtractor(f).nRecords
-        else:
-            if ReadExtractor(f).nRecords != nrecords:
-                print "Inconsistent numbers of records between files"
-                sys.exit(1)
-    # Generate indexes for a random subset
-    rand_records = []
-    while len(rand_records) < N:
-        r = random.randint(0,nrecords-1)
-        if r not in rand_records: rand_records.append(r)
-    # Loop over files again
-    for f in arguments:
-        s = ReadExtractor(f)
-        s.subset(rand_records)
+    if options.pattern:
+        # Do pattern matching
+        print "Extracting reads that match '%s'" % options.pattern
+        for f in arguments:
+            s = ReadExtractor(f)
+            s.regex_subset(options.pattern)
+        sys.exit()
+    else:
+        # Extract random reads
+        N = options.N
+        print "Extracting %d unique random reads from input files" % N
+        # Loop over input files and collect data
+        nrecords = None
+        for f in arguments:
+            if nrecords is None:
+                nrecords = ReadExtractor(f).nRecords
+            else:
+                if ReadExtractor(f).nRecords != nrecords:
+                    print "Inconsistent numbers of records between files"
+                    sys.exit(1)
+        # Generate indexes for a random subset
+        rand_records = []
+        while len(rand_records) < N:
+            r = random.randint(0,nrecords-1)
+            if r not in rand_records: rand_records.append(r)
+        # Loop over files again
+        for f in arguments:
+            s = ReadExtractor(f)
+            s.subset(rand_records)
         
     
