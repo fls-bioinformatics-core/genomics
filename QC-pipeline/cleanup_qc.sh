d329afcfbd13ef4d521324f25e531fdf82b7e16e
Peter Briggs <peter.briggs@manchester.ac.uk>
2012-05-15 14:05:11 +0100

    QC-pipeline/cleanup_qc.sh: utility to remove all QC pipeline products in current directory.

---
+++ QC-pipeline/cleanup_qc.sh
@@ -0,0 +1,18 @@
+#/bin/sh
+#
+# Utility script to clean up qc products
+echo -n "Remove all QC products in `pwd`? [y/N]: "
+read ok
+case $ok in
+    y|Y|yes|Yes)
+	# Do clean up
+	/bin/rm -r qc
+	/bin/rm *_T_F3.*
+	/bin/rm *.fastq
+	/bin/rm SOLiD_preprocess_filter.stats SOLiD_preprocess_filter_paired.stats
+	/bin/rm qc.*.o* qc.*.e*
+	;;
+esac
+echo "Done"
+##
+#
