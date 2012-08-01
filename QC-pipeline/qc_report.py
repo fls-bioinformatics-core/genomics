43ebdbf8928d8dd2b9525e9d3b7429af7eff89da
Peter Briggs <peter.briggs@manchester.ac.uk>
2012-08-01 16:59:25 +0100

    qc_report.py: more updating of styles.

--- QC-pipeline/qc_report.py
+++ QC-pipeline/qc_report.py
@@ -165,18 +165,21 @@ class SolidQCReport:
         html.add("<h1>QC for %s</h1>" % os.path.basename(self.__dirn))
         # Add styles
         html.addCSSRule("h1 { background-color: #42AEC2;\n"
-                        "     color: white; }")
+                        "     color: white;\n"
+                        "     padding: 5px 10px; }")
         html.addCSSRule("h2 { background-color: #8CC63F;\n"
                         "     color: white;\n"
                         "     display: inline-block;\n"
-                        "     padding: 5px 5px;\n"
+                        "     padding: 5px 15px;\n"
                         "     margin: 0;\n"
-                        "     border-top-left-radius: 20; }")
+                        "     border-top-left-radius: 20;\n"
+                        "     border-bottom-right-radius: 20; }")
         html.addCSSRule(".sample { margin: 10 10;\n"
                         "          border: solid 2px #8CC63F;\n"
                         "          padding: 0;\n"
                         "          background-color: #ffe;\n"
-                        "          border-top-left-radius: 25; }")
+                        "          border-top-left-radius: 25;\n"
+                        "          border-bottom-right-radius: 25; }")
         html.addCSSRule("table.summary { border: solid 1px grey;\n"
                         "                font-size: 90% }")
         html.addCSSRule("table.summary th { background-color: grey;\n"
