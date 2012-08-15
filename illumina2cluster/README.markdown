b82e8874dd743488ceec7dbb9707d347356bc895
Peter Briggs <peter.briggs@manchester.ac.uk>
2012-08-15 14:17:13 +0100

    illumina2cluster: new utility update_sample_sheet.py for editing Illumina GA2 SampleSheet.csv files before running bcl to fastq conversion.

--- illumina2cluster/README.markdown
+++ illumina2cluster/README.markdown
@@ -4,6 +4,7 @@ illumina2cluster
 Utilities for preparing data on the cluster from the Illumina instrument:
 
  *   `bclToFastq.sh`: generate FASTQ from BCL files
+ *   `update_sample_sheet.py`: 
 
 
 bclToFastq.sh
@@ -23,3 +24,39 @@ files).
 The script runs `configureBclToFastq.pl` from `CASAVA` to set up conversion scripts,
 then runs `make` to perform the actual conversion. It requires that `CASAVA` is available
 on the system.
+
+
+update_sample_sheet.py
+----------------------
+
+View and manipulate sample sheet files for Illumina GA2 sequencer.
+
+Usage:
+
+    update_sample_sheet.py [OPTIONS] SampleSheet.csv
+
+Utility to view and edit SampleSheet file from Illumina GA2 sequencer. Can be
+used to update sample IDs and project names before running BCL to FASTQ
+conversion.
+
+Options:
+
+    -h, --help            show this help message and exit
+    -o SAMPLESHEET_OUT    output new sample sheet to SAMPLESHEET_OUT
+    -v, --view            view contents of sample sheet
+    --set-id=SAMPLE_ID    update/set the values in the 'SampleID' field;
+                          SAMPLE_ID should be of the form '<lanes>:<name>',
+                          where <lanes> is a single integer (e.g. 1), a set of
+                          integers (e.g. 1,3,...), a range (e.g. 1-3), or a
+                          combination (e.g. 1,3-5,7).
+    --set-project=SAMPLE_PROJECT
+                          update/set values in the 'SampleProject' field;
+                          SAMPLE_PROJECT should be of the form '<lanes>:<name>',
+                          where <lanes> is a single integer (e.g. 1), a set of
+                          integers (e.g. 1,3,...), a range (e.g. 1-3), or a
+                          combination (e.g. 1,3-5,7).
+
+Example:
+
+    update_sample_sheet.py -o SampleSheet2.csv --set-project=1,8:Control \
+        --set-id=1:PhiX_10pM --set-id=8:PhiX_12pM SampleSheet.csv
---
+++ illumina2cluster/update_sample_sheet.py
@@ -0,0 +1,128 @@
+#!/bin/env python
+#
+#     update_sample_sheet.py: edit sample sheet files from Illumina sequencer
+#     Copyright (C) University of Manchester 2012 Peter Briggs
+#
+########################################################################
+#
+# update_sample_sheet.py
+#
+#########################################################################
+
+"""update_sample_sheet.py
+
+View and manipulate sample sheet files for Illumina GA2 sequencer.
+"""
+
+#######################################################################
+# Imports
+#######################################################################
+
+import os
+import sys
+import optparse
+# Put ../share onto Python search path for modules
+SHARE_DIR = os.path.abspath(
+    os.path.normpath(
+        os.path.join(os.path.dirname(sys.argv[0]),'..','share')))
+sys.path.append(SHARE_DIR)
+import TabFile
+
+#######################################################################
+# Functions
+#######################################################################
+
+def parse_name_expression(name_expr):
+    """Break up a 'name expression' into lane numbers and associated name
+
+    name_expr: a string of the form '<lanes>:<name>', where <lanes> can
+    be a single integer (e.g. 1), a set of comma-separated integers (e.g.
+    1,2,3), a range (e.g. 1-4), or a combination (e.g. 1,3,5-8).
+
+    Returns a tuple (lanes,name) where lanes is a Python list of integers
+    representing lanes, and name is a string with the associated name.
+    """
+    # Name expressions are of the form 'expr:name'
+    try:
+        # Extract components
+        i = str(name_expr).index(':')
+        expr = str(name_expr)[:i]
+        name = str(name_expr)[i+1:]
+    except ValueError:
+        raise Exception, "name expression '%s' has incorrect format" % name_expr
+    # Extract lane numbers from leading expression
+    fields = expr.split(',')
+    lanes = []
+    for field in fields:
+        # Check for ranges i.e. 1-3
+        try:
+            i = field.index('-')
+            l1 = int(field[:i])
+            l2 = int(field[i+1:])
+            for i in xrange(l1,l2+1): lanes.append(i)
+        except ValueError:
+            # Not a range
+            lanes.append(int(field))
+    # Sort into order
+    lanes.sort()
+    # Return tuple
+    return (lanes,name)
+
+#######################################################################
+# Main program
+#######################################################################
+
+if __name__ == "__main__":
+    # Set up option parser
+    p = optparse.OptionParser(usage="%prog [OPTIONS] SampleSheet.csv",
+                              description="Utility to view and edit SampleSheet file from "
+                              "Illumina GA2 sequencer. Can be used to update sample IDs and "
+                              "project names before running BCL to FASTQ conversion.")
+    p.add_option('-o',action="store",dest="samplesheet_out",default=None,
+                 help="output new sample sheet to SAMPLESHEET_OUT")
+    p.add_option('-v','--view',action="store_true",dest="view",
+	         help="view contents of sample sheet")
+    p.add_option('--set-id',action="append",dest="sample_id",default=[],
+                 help="update/set the values in the 'SampleID' field; "
+                 "SAMPLE_ID should be of the form '<lanes>:<name>', where <lanes> is a single "
+                 "integer (e.g. 1), a set of integers (e.g. 1,3,...), a range (e.g. 1-3), or "
+                 "a combination (e.g. 1,3-5,7).")
+    p.add_option('--set-project',action="append",dest="sample_project",default=[],
+                 help="update/set values in the 'SampleProject' field; "
+                 "SAMPLE_PROJECT should be of the form '<lanes>:<name>', where <lanes> is a "
+                 "single integer (e.g. 1), a set of integers (e.g. 1,3,...), a range (e.g. 1-3), "
+                 "or a combination (e.g. 1,3-5,7).")
+    # Process command line
+    options,args = p.parse_args()
+    if len(args) != 1:
+        p.error("input is a single SampleSheet.csv file")
+    # Get input sample sheet file
+    samplesheet = args[0]
+    if not os.path.isfile(samplesheet):
+        sys.stderr("Sample sheet '%s': not found" % samplesheet)
+        sys.exit(1)
+    # Read in the data as CSV
+    data = TabFile.TabFile(samplesheet,delimiter=',',first_line_is_header=False,
+                           column_names=('FCID','Lane','SampleID','SampleRef',
+                                         'Index','Description','Control',
+                                         'Recipe','Operator','SampleProject'))
+    # Update the SampleID and SampleProject fields
+    for sample_id in options.sample_id:
+        lanes,name = parse_name_expression(sample_id)
+        for lane in lanes:
+            print "Setting SampleID for lane %d: '%s'" % (lane,name)
+            data[lane]['SampleID'] = name
+    # Update the SampleProject field
+    for sample_project in options.sample_project:
+        lanes,name = parse_name_expression(sample_project)
+        for lane in lanes:
+            print "Setting SampleProject for lane %d: '%s'" % (lane,name)
+            data[lane]['SampleProject'] = name
+    # Print transposed data in tab-delimited format
+    if options.view:
+        data.transpose().write(fp=sys.stdout,delimiter='\t')
+    # Write out new sample sheet
+    if options.samplesheet_out:
+        data.write(options.samplesheet_out)
+    sys.exit()
+    
