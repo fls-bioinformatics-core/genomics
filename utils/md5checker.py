8bc3fb8e7afe973b36d57f083bdbb81e6b713a25
Peter Briggs <peter.briggs@manchester.ac.uk>
2012-09-11 15:49:27 +0100

    md5checker.py: updated version to 0.2.2.

--- utils/md5checker.py
+++ utils/md5checker.py
@@ -18,7 +18,7 @@ Utility for checking files and directories using md5 checksums.
 # Module metadata
 #######################################################################
 
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 
 #######################################################################
 # Import modules that this module depends on
