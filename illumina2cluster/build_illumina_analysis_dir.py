e74a63ad807e6ebfa646b72cfa03ccd0769373de
Peter Briggs <peter.briggs@manchester.ac.uk>
2012-11-26 16:20:00 +0000

    illumina2cluster/build_illumina_analysis_dir.py: make relative sym links to source fastq files when building analysis directories.

--- illumina2cluster/build_illumina_analysis_dir.py
+++ illumina2cluster/build_illumina_analysis_dir.py
@@ -300,6 +300,45 @@ def concatenate_fastq_files(merged_fastq,fastq_files):
     fq_merged.close()
     os.rename(merged_fastq_part,merged_fastq)
 
+def mklink(target,link_name,relative=False):
+    """Make a symbolic link
+
+    Arguments:
+      target: the file or directory to link to
+      link_name: name of the link
+      relative: if True then make a relative link (if possible);
+        otherwise link to the target as given (default)"""
+    logging.debug("Linking to %s from %s" % (target,link_name))
+    target_path = target
+    if relative:
+        # Try to construct relative link to target
+        target_abs_path = os.path.abspath(target)
+        link_abs_path = os.path.abspath(link_name)
+        common_prefix = commonprefix(target_abs_path,link_abs_path)
+        if common_prefix:
+            # Use relpath to generate the relative path from the link
+            # to the target
+            target_path = os.path.relpath(target_abs_path,os.path.dirname(link_abs_path))
+    os.symlink(target_path,link_name)
+
+def commonprefix(path1,path2):
+    """Determine common prefix path for path1 and path2
+
+    Can't use os.path.commonprefix as it checks characters not
+    path components, so essentially it doesn't work as required.
+    """
+    path1_components = str(path1).split(os.sep)
+    path2_components = str(path2).split(os.sep)
+    common_components = []
+    ncomponents = min(len(path1_components),len(path2_components))
+    for i in range(ncomponents):
+        if path1_components[i] == path2_components[i]:
+            common_components.append(path1_components[i])
+        else:
+            break
+    commonprefix = "%s" % os.sep.join(common_components)
+    return commonprefix
+
 #######################################################################
 # Main program
 #######################################################################
@@ -388,7 +427,7 @@ if __name__ == "__main__":
                         print "-> %s.fastq.gz already exists" % sample.name
                     else:
                         print "Linking to %s" % fastq
-                        if not options.dry_run: os.symlink(fastq_file,fastq_ln)
+                        if not options.dry_run: mklink(fastq_file,fastq_ln,relative=True)
         else:
             # Merge files for replicates within each sample
             for sample in project.samples:
