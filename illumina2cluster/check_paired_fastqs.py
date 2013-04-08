4e741174b16a0f88d992877e377042ea1cbdbc9a
Peter Briggs <peter.briggs@manchester.ac.uk>
2013-04-08 17:31:23 +0100

    illumina2cluster.py/check_paired_fastqs.py: updated comments and docstrings.

--- illumina2cluster/check_paired_fastqs.py
+++ illumina2cluster/check_paired_fastqs.py
@@ -1,27 +1,80 @@
 #!/bin/env python
 #
+#     check_paired_fastqs.py: verify that R1/R2 fastq pair are consistent
+#     Copyright (C) University of Manchester 2013 Peter Briggs
+#
+########################################################################
+#
+# check_paired_fastqs.py
+#
+#########################################################################
+
+"""check_paired_fastqs.py
+
+Verify that an R1/R2 pair of FASTQ files is consistent, specifically:
+
+1. That they both contain the same number of reads, and
+2. That the read headers for each read contain the same information
+   in the same order.
+
+"""
+
+#######################################################################
+# Import modules that this module depends on
+#######################################################################
+
+__version__ = "0.0.1"
+
 import os
 import sys
 import optparse
+import itertools
+import logging
+
+# Put ../share onto Python search path for modules
+SHARE_DIR = os.path.abspath(
+    os.path.normpath(
+        os.path.join(os.path.dirname(sys.argv[0]),'..','share')))
+sys.path.append(SHARE_DIR)
 import IlluminaData
 import FASTQFile
-import logging
-import itertools
-#
+
+#######################################################################
+# Module Functions
+#######################################################################
+
 def get_nreads(fastq_file):
+    """Return number of reads in a FASTQ file
+
+    Simple-minded read count performed by counting the number of lines
+    in the file and dividing by 4.
+
+    Arguments:
+      fastq_file: file to get read count for
+
+    Returns:
+      Number of reads.
+
+    """
     nreads = 0
     fp = open(fastq_file,'rU')
     for line in fp:
         nreads += 1
     fp.close()
     return nreads
-#
+
+#######################################################################
+# Main program
+#######################################################################
+
 if __name__ == "__main__":
     
     # Create command line parser
-    p = optparse.OptionParser(usage="%prog OPTIONS R1.fastq R2.fastq",
+    p = optparse.OptionParser(usage="%prog OPTIONS R1_fastq R2_fastq",
+                              version="%prog "+__version__,
                               description="Check that read headers for R1 and R2 fastq files "
-                              "are in agreement for all reads")
+                              "are in agreement for all reads. Input files can be either fastq "
+                              "or fastq.gz.")
     # Parse command line
     options,args = p.parse_args()
     # Get data directory name
