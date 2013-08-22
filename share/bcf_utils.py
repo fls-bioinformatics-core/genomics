d2c315f16c5697595ec02aaf994a9553ba584204
Peter Briggs <peter.briggs@manchester.ac.uk>
2013-08-22 16:32:12 +0100

    share/bcf_utils.py: 1.0.2, update concatenate_fastq_files to improve performance.

--- share/bcf_utils.py
+++ share/bcf_utils.py
@@ -7,7 +7,7 @@
 #
 #########################################################################
 
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 
 """bcf_utils
 
@@ -20,6 +20,7 @@ Basic file system wrappers and utilities:
   chmod
   format_file_size
   commonprefix
+  is_gzipped_file
 
 Sample name utilities:
 
@@ -43,6 +44,7 @@ import os
 import logging
 import string
 import gzip
+import shutil
 
 #######################################################################
 # Class definitions
@@ -161,6 +163,18 @@ def commonprefix(path1,path2):
             break
     commonprefix = "%s" % os.sep.join(common_components)
     return commonprefix
+
+def is_gzipped_file(filename):
+    """Check if a file has a .gz extension
+
+    Arguments:
+      filename: name of the file to be tested (can include leading path)
+
+    Returns:
+      True if filename has trailing .gz extension, False if not.
+
+    """
+    return os.path.splitext(filename)[1] == '.gz'
 
 # Sample/library name utilities
 
@@ -288,7 +302,8 @@ def pretty_print_names(name_list):
 
 # File manipulations
 
-def concatenate_fastq_files(merged_fastq,fastq_files,bufsize=10240):
+def concatenate_fastq_files(merged_fastq,fastq_files,bufsize=10240,
+                            overwrite=False,verbose=True):
     """Create a single FASTQ file by concatenating one or more FASTQs
 
     Given a list or tuple of FASTQ files (which can be compressed or
@@ -299,31 +314,49 @@ def concatenate_fastq_files(merged_fastq,fastq_files,bufsize=10240):
       merged_fastq: name of output FASTQ file (mustn't exist beforehand)
       fastq_files:  list of FASTQ files to concatenate
       bufsize: (optional) size of buffer to use for copying data
+      overwrite: (optional) if True then overwrite the output file if it
+        already exists (otherwise raise OSError); default is False
+      verbose: (optional) if True then report operations to stdout,
+        otherwise operate quietly
 
     """
-    print "Creating merged fastq file '%s'" % merged_fastq
+    if verbose: print "Creating merged fastq file '%s'" % merged_fastq
     # Check that initial file doesn't exist
-    if os.path.exists(merged_fastq):
+    if os.path.exists(merged_fastq) and not overwrite:
         raise OSError, "Target file '%s' already exists, stopping" % merged_fastq
     # Create temporary name
     merged_fastq_part = merged_fastq+'.part'
     # Open for writing
-    if os.path.splitext(merged_fastq)[1] == ".gz":
-        # Output file is gzipped
-        fq_merged = gzip.GzipFile(merged_fastq_part,'wb')
+    if is_gzipped_file(merged_fastq):
+        if is_gzipped_file(fastq_files[0]):
+            # Copy first file in list directly and open for append
+            if verbose: print "Copying %s" % fastq_files[0]
+            shutil.copy(fastq_files[0],merged_fastq_part)
+            first_file = 1
+            fq_merged = gzip.GzipFile(merged_fastq_part,'ab')
+        else:
+            # Open for write
+            first_file = 0
+            fq_merged = gzip.GzipFile(merged_fastq_part,'wb')
     else:
-        # Assume regular file
-        fq_merged = open(merged_fastq_part,'wb')
+        if not is_gzipped_file(fastq_files[0]):
+            if verbose: print "Copying %s" % fastq_files[0]
+            # Copy first file in list directly and open for append
+            shutil.copy(fastq_files[0],merged_fastq_part)
+            first_file = 1
+            fq_merged = open(merged_fastq_part,'ab')
+        else:
+            # Assume regular file
+            first_file = 1
+            fq_merged = open(merged_fastq_part,'wb')
     # For each fastq, read data and append to output - simples!
-    for fastq in fastq_files:
-        print "Adding records from %s" % fastq
+    for fastq in fastq_files[first_file:]:
+        if verbose: print "Adding records from %s" % fastq
         # Check it exists
         if not os.path.exists(fastq):
             raise OSError, "'%s' not found, stopping" % fastq
-        # Check if it's compressed i.e. gz extension?
-        gzipped = (os.path.splitext(fastq)[1] == ".gz")
         # Open file for reading
-        if not gzipped:
+        if not is_gzipped_file(fastq):
             fq = open(fastq,'rb')
         else:
             fq = gzip.GzipFile(fastq,'rb')
@@ -352,6 +385,14 @@ class TestFileSystemFunctions(unittest.TestCase):
                                                    '/mnt/stuff/dir2'))
         self.assertEqual('',commonprefix('/mnt1/stuff/dir1',
                                          '/mnt2/stuff/dir2'))
+
+    def test_is_gzipped_file(self):
+        self.assertTrue(is_gzipped_file('hello.gz'))
+        self.assertTrue(is_gzipped_file('hello.tar.gz'))
+        self.assertFalse(is_gzipped_file('hello'))
+        self.assertFalse(is_gzipped_file('hello.txt'))
+        self.assertFalse(is_gzipped_file('hello.gz.part'))
+        self.assertFalse(is_gzipped_file('hellogz'))
 
 class TestFormatFileSize(unittest.TestCase):
     """Unit tests for formatting file sizes
@@ -404,6 +445,73 @@ class TestNameFunctions(unittest.TestCase):
         self.assertEqual('JC_SEQ26, JC_SEQ28, JC_SEQ30',pretty_print_names(('JC_SEQ26',
                                                            'JC_SEQ28',
                                                            'JC_SEQ30')))
+
+class TestConcatenateFastqFiles(unittest.TestCase):
+    """Unit tests for concatenate_fastq_files
+
+    """
+    def setUp(self):
+        # Create a set of test files
+        self.fastq_data1 = """"@73D9FA:3:FC:1:1:7507:1000 1:N:0:
+NACAACCTGATTAGCGGCGTTGACAGATGTATCCAT
++
+#))))55445@@@@@C@@@@@@@@@:::::<<:::<
+@73D9FA:3:FC:1:1:15740:1000 1:N:0:
+NTCTTGCTGGTGGCGCCATGTCTAAATTGTTTGGAG
++
+#+.))/0200<<<<<:::::CC@@C@CC@@@22@@@
+@73D9FA:3:FC:1:1:8103:1000 1:N:0:
+NGACCGATTAGAGGCGTTTTATGATAATCCCAATGC
++
+#(,((,)*))/.0--2255282299@@@@@@@@@@@
+"""
+        self.fastq_data2 = """@73D9FA:3:FC:1:1:7488:1000 1:N:0:
+NTGATTGTCCAGTTGCATTTTAGTAAGCTCTTTTTG
++
+#,,,,33223CC@@@@@@@C@@@@@@@@C@CC@222
+@73D9FA:3:FC:1:1:6680:1000 1:N:0:
+NATAAATCACCTCACTTAAGTGGCTGGAGACAAATA
++
+#--,,55777@@@@@@@CC@@C@@@@@@@@:::::<
+"""
+
+    def tearDown(self):
+        os.remove(self.fastq1)
+        os.remove(self.fastq2)
+        os.remove(self.merged_fastq)
+
+    def make_fastq_file(self,fastq,data):
+        # Create a fastq file for the testing
+        if os.path.splitext(fastq)[1] != '.gz':
+            open(fastq,'wt').write(data)
+        else:
+            gzip.GzipFile(fastq,'wt').write(data)
+
+    def test_concatenate_fastq_files(self):
+        self.fastq1 = "concat.unittest.1.fastq"
+        self.fastq2 = "concat.unittest.2.fastq"
+        self.make_fastq_file(self.fastq1,self.fastq_data1)
+        self.make_fastq_file(self.fastq2,self.fastq_data2)
+        self.merged_fastq = "concat.unittest.merged.fastq"
+        concatenate_fastq_files(self.merged_fastq,
+                                [self.fastq1,self.fastq2],
+                                overwrite=True,
+                                verbose=False)
+        merged_fastq_data = open(self.merged_fastq,'r').read()
+        self.assertEqual(merged_fastq_data,self.fastq_data1+self.fastq_data2)
+
+    def test_concatenate_fastq_files_gzipped(self):
+        self.fastq1 = "concat.unittest.1.fastq.gz"
+        self.fastq2 = "concat.unittest.2.fastq.gz"
+        self.make_fastq_file(self.fastq1,self.fastq_data1)
+        self.make_fastq_file(self.fastq2,self.fastq_data2)
+        self.merged_fastq = "concat.unittest.merged.fastq.gz"
+        concatenate_fastq_files(self.merged_fastq,
+                                [self.fastq1,self.fastq2],
+                                overwrite=True,
+                                verbose=False)
+        merged_fastq_data = gzip.GzipFile(self.merged_fastq,'r').read()
+        self.assertEqual(merged_fastq_data,self.fastq_data1+self.fastq_data2)
 
 #######################################################################
 # Main program
