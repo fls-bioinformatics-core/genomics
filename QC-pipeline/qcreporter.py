353f370799e18597535ee67217df0b50a70bd570
Peter Briggs <peter.briggs@manchester.ac.uk>
2014-05-07 16:11:34 +0100

    Moved HTMLPageWriter into new module share/htmlpagewriter (from QC-pipeline/qcreporter.py).

--- QC-pipeline/qcreporter.py
+++ QC-pipeline/qcreporter.py
@@ -14,7 +14,7 @@
 Generate HTML reports for an NGS QC pipeline runs.
 """
 
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 
 #######################################################################
 # Import modules that this module depends on
@@ -37,6 +37,7 @@ try:
     import Pipeline
     import TabFile
     import bcf_utils
+    import htmlpagewriter
 except ImportError, ex:
     print "Error importing modules: %s" % ex
     print "Check PYTHONPATH"
@@ -188,7 +189,7 @@ class QCReporter:
         """Internal: initialise and populate the HTMLPageWriter
         """
         # Set up HTML document
-        html = HTMLPageWriter("%s/%s: QC report" % (self.run,self.name))
+        html = htmlpagewriter.HTMLPageWriter("%s/%s: QC report" % (self.run,self.name))
         # Title
         html.add("<h1>%s/%s: QC report</h1>" % (self.run,self.name))
         # Info on reporter version and date
@@ -483,7 +484,8 @@ class QCSample:
                 # Add Images
                 if inline_pngs:
                     pngdata = "data:image/png;base64," + \
-                        PNGBase64Encoder().encodePNG(os.path.join(self.qc_dir,s))
+                        htmlpagewriter.PNGBase64Encoder().encodePNG(
+                            os.path.join(self.qc_dir,s))
                 else:
                     pngdata = os.path.join(qc_dir,s)
                 html_content="<a href='qc/%s'><img src='%s' height=250 /></a>" % (s,pngdata)
@@ -523,7 +525,8 @@ class QCSample:
                 # Add images
                 if inline_pngs:
                     pngdata = "data:image/png;base64," + \
-                        PNGBase64Encoder().encodePNG(os.path.join(self.qc_dir,b))
+                        htmlpagewriter.PNGBase64Encoder().encodePNG(
+                            os.path.join(self.qc_dir,b))
                 else:
                     pngdata = os.path.join(self.qc_dir,b)
                 html_content=\
@@ -1154,127 +1157,6 @@ class SolidQCSample(QCSample):
             if len(self.screens()) != 3:
                 logging.warning("%s: wrong number of screens" % self.name)
         return status
-# 
-class HTMLPageWriter:
-    """Generic HTML generation class
-
-    HTMLPageWriter provides basic operations for writing HTML
-    files.
-
-    Example usage:
-
-    >>> p = HTMLPageWriter("Example page")
-    >>> p.add("This is some text")
-    >>> p.write("example.html")
-    """
-
-    def __init__(self,title=''):
-        """Create a new HTMLPageWriter instance
-
-        Arguments:
-          title: optional title for the HTML document
-        """
-        self.__page_title = str(title)
-        self.__content = []
-        self.__css_rules = []
-        self.__javascript = []
-
-    def add(self,content):
-        """Add content to page body
-
-        Note that the supplied content is added to the HTML
-        document as-is; no escaping is performed so the content
-        can include arbitrary HTML tags. Note also that no
-        validation is performed.
-
-        Arguments:
-          content: text to add to the HTML document body
-        """
-        self.__content.append(str(content))
-
-    def addCSSRule(self,css_rule):
-        """Add CSS rule
-
-        Defines a CSS rule that will be inlined into a
-        "style" tag in the HTML head when the document is
-        written out.
-
-        The rule text is added as-is, e.g.:
-
-        >>> p = HTMLPageWriter("Example page")
-        >>> p.addCSSRule("body { color: blue; }")
-
-        No checking or validation is performed.
-
-        Arguments:
-          css_rule: text defining CSS rule
-        """
-        self.__css_rules.append(str(css_rule))
-
-    def addJavaScript(self,javascript):
-        """Add JavaScript
-
-        Defines a line of Javascript code that will be
-        inlined into a "script" tag in the HTML head when
-        the document is written out.
-
-        The code is added as-is, no checking or validation
-        is performed.
-
-        Arguments:
-          javascript: Javascript code
-        """
-        self.__javascript.append(str(javascript))
-
-    def write(self,filen):
-        """Write the HTML document to file
-
-        Generates a HTML document based on the content, styles
-        etc that have been defined by calls to the object's
-        methods.
-
-        Arguments:
-          filen: name of the file to write the document to
-        """
-        fp = open(filen,'w')
-        fp.write("<html>\n")
-        # Header
-        fp.write("<head>\n")
-        fp.write("<title>%s</title>\n" % self.__page_title)
-        # CSS rules
-        if self.__css_rules:
-            fp.write("<style type=\"text/css\">\n")
-            fp.write('\n'.join(self.__css_rules))
-            fp.write("</style>\n")
-        # JavaScript
-        if self.__javascript:
-            fp.write("<script language='javascript' type='text/javascript'><!--\n")
-            fp.write('\n'.join(self.__javascript))
-            fp.write("\n--></script>\n")
-        fp.write("<head>\n")
-        # Body and content
-        fp.write("<body>\n")
-        fp.write('\n'.join(self.__content))
-        fp.write("</body>\n")
-        # Finish
-        fp.write("</html>\n")
-        fp.close()
-
-# Utility class to encode PNGs for embedding in HTML
-class PNGBase64Encoder:
-    """Utility class to encode PNG file into a base64 string
-
-    Base64 encoded PNGs can be embedded in HTML <img> tags.
-
-    To use:
-
-    >>> p = PNGBase64Encoder.encodePNG("image.png")
-    """
-
-    def encodePNG(self,pngfile):
-        """Return base64 string encoding a PNG file.
-        """
-        return base64.b64encode(open(pngfile,'rb').read())
 
 #######################################################################
 # Functions
