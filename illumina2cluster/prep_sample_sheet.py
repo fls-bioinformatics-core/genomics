4ba537c5d14fdff814907657543b94a62c5df1dd
Peter Briggs <peter.briggs@manchester.ac.uk>
2014-06-02 13:41:29 +0100

    illumina2cluster/prep_sample_sheet.py: 0.2.1, new options --include-lanes and --truncate-barcodes (plus some refactoring and addition of tests).

--- illumina2cluster/prep_sample_sheet.py
+++ illumina2cluster/prep_sample_sheet.py
@@ -15,7 +15,7 @@ Prepare sample sheet file for Illumina sequencers.
 
 """
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 #######################################################################
 # Imports
@@ -55,7 +55,22 @@ def parse_name_expression(name_expr):
     except ValueError:
         raise Exception, "name expression '%s' has incorrect format" % name_expr
     # Extract lane numbers from leading expression
-    fields = expr.split(',')
+    lanes = parse_lane_expression(expr)
+    # Return tuple
+    return (lanes,name)
+
+def parse_lane_expression(lane_expr):
+    """Break up a 'lane expression' into a list of lane numbers
+
+    lane_expr: a string consisting of a single integer (e.g. 1), a set of
+    comma-separated integers (e.g. 1,2,3), a range (e.g. 1-4), or a
+    combination (e.g. 1,3,5-8).
+
+    Returns a list of integers representing lane numbers.
+
+    """
+    # Extract lane numbers
+    fields = lane_expr.split(',')
     lanes = []
     for field in fields:
         # Check for ranges i.e. 1-3
@@ -69,8 +84,77 @@ def parse_name_expression(name_expr):
             lanes.append(int(field))
     # Sort into order
     lanes.sort()
-    # Return tuple
-    return (lanes,name)
+    return lanes
+
+def truncate_barcode(seq,length):
+    """Return barcode sequence truncated to requested length
+
+    'seq' is a barcode sequence (note that dual index sequences
+    are of the form e.g. 'AGGTAC-GGCCTT' i.e. the name includes
+    a hyphen) and 'length' is the desired length (i.e. number of
+    bases to keep).
+
+    """
+    try:
+        i = seq.index('-')
+        # Dual index barcode
+        if i >= length:
+            return seq[:length]
+        else:
+            return seq[:length+1]
+    except ValueError:
+        # No hyphen: single index barcode
+        return seq[:length]
+
+#######################################################################
+# Unit tests
+#######################################################################
+
+import unittest
+
+class TestParseNameExpressionFunction(unittest.TestCase):
+    """Tests for the 'parse_name_expression' function
+
+    """
+    def test_parse_single_lane(self):
+        self.assertEqual(parse_name_expression("1:Test"),([1],'Test'))
+    def test_parse_list_of_lanes(self):
+        self.assertEqual(parse_name_expression("1,2,4:Test"),([1,2,4],'Test'))
+    def test_parse_range_of_lanes(self):
+        self.assertEqual(parse_name_expression("2-4:Test"),([2,3,4],'Test'))
+    def test_parse_mixtures_of_list_and_range(self):
+        self.assertEqual(parse_name_expression("1-3,5:Test"),([1,2,3,5],'Test'))
+        self.assertEqual(parse_name_expression("1,3-5:Test"),([1,3,4,5],'Test'))
+
+class TestParseLaneExpressionFunction(unittest.TestCase):
+    """Tests for the 'parse_lane_expression' function
+
+    """
+    def test_parse_single_lane(self):
+        self.assertEqual(parse_lane_expression("1"),[1])
+    def test_parse_list_of_lanes(self):
+        self.assertEqual(parse_lane_expression("1,2,4"),[1,2,4])
+    def test_parse_range_of_lanes(self):
+        self.assertEqual(parse_lane_expression("2-4"),[2,3,4])
+    def test_parse_mixtures_of_list_and_range(self):
+        self.assertEqual(parse_lane_expression("1-3,5"),[1,2,3,5])
+        self.assertEqual(parse_lane_expression("1,3-5"),[1,3,4,5])
+
+class TestTruncateBarcodeFunction(unittest.TestCase):
+    """Tests for the 'truncate_barcode' function
+
+    """
+    def test_truncate_single_index_barcode(self):
+        self.assertEqual(truncate_barcode('CGTACTAG',0),'')
+        self.assertEqual(truncate_barcode('CGTACTAG',6),'CGTACT')
+        self.assertEqual(truncate_barcode('CGTACTAG',8),'CGTACTAG')
+        self.assertEqual(truncate_barcode('CGTACTAG',10),'CGTACTAG')
+    def test_truncate_dual_index_barcode(self):
+        self.assertEqual(truncate_barcode('AGGCAGAA-TAGATCGC',0),'')
+        self.assertEqual(truncate_barcode('AGGCAGAA-TAGATCGC',6),'AGGCAG')
+        self.assertEqual(truncate_barcode('AGGCAGAA-TAGATCGC',8),'AGGCAGAA')
+        self.assertEqual(truncate_barcode('AGGCAGAA-TAGATCGC',10),'AGGCAGAA-TA')
+        self.assertEqual(truncate_barcode('AGGCAGAA-TAGATCGC',16),'AGGCAGAA-TAGATCGC')
 
 #######################################################################
 # Main program
@@ -112,6 +196,15 @@ if __name__ == "__main__":
     p.add_option('--ignore-warnings',action="store_true",dest="ignore_warnings",default=False,
                  help="ignore warnings about spaces and duplicated sampleID/sampleProject "
                  "combinations when writing new samplesheet.csv file")
+    p.add_option('--include-lanes',action="store",dest="lanes",default=None,
+                 help="specify a subset of lanes to include in the output sample sheet; "
+                 "LANES should be single integer (e.g. 1), a list of integers (e.g. "
+                 "1,3,...), a range (e.g. 1-3) or a combination (e.g. 1,3-5,7). Default "
+                 "is to include all lanes")
+    p.add_option('--truncate-barcodes',action="store",dest="barcode_len",default=None,
+                 type='int',
+                 help="trim barcode sequences in sample sheet to number of bases specified "
+                 "by BARCODE_LEN. Default is to leave barcode sequences unmodified")
     deprecated_options = optparse.OptionGroup(p,"Deprecated options")
     deprecated_options.add_option('--miseq',action="store_true",dest="miseq",
                                   help="convert input MiSEQ sample sheet to CASAVA-compatible "
@@ -132,6 +225,16 @@ if __name__ == "__main__":
         sys.exit(1)
     # Read in the data as CSV
     data = IlluminaData.get_casava_sample_sheet(samplesheet)
+    # Remove lanes
+    if options.lanes is not None:
+        lanes = parse_lane_expression(options.lanes)
+        print "Keeping lanes %s, removing the rest" % ','.join([str(x) for x in lanes])
+        new_data = IlluminaData.CasavaSampleSheet()
+        for line in data:
+            if line['Lane'] in lanes:
+                print "Keeping %s" % line
+                new_data.append(tabdata="%s" % line)
+        data = new_data
     # Update the SampleID and SampleProject fields
     for sample_id in options.sample_id:
         lanes,name = parse_name_expression(sample_id)
@@ -146,6 +249,18 @@ if __name__ == "__main__":
             if line['Lane'] in lanes:
                 print "Setting SampleProject for lane %d: '%s'" % (line['Lane'],name)
                 line['SampleProject'] = name
+    # Truncate barcodes
+    if options.barcode_len is not None:
+        barcode_len = options.barcode_len
+        for line in data:
+            barcode = truncate_barcode(line['Index'],options.barcode_len)
+            print "Lane %d '%s/%s': barcode '%s' -> '%s'" \
+                % (line['Lane'],
+                   line['SampleProject'],
+                   line['SampleID'],
+                   line['Index'],
+                   barcode)
+            line['Index'] = barcode
     # Fix spaces
     if options.fix_spaces:
         data.fix_illegal_names()
