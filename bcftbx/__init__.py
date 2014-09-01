5c984d06cb0c69887af4205ee08492f984e4300d
Peter Briggs <peter.briggs@manchester.ac.uk>
2014-09-01 14:25:32 +0100

    bcftbx/__init__.py: set package version to 0.99.0 and add 'get_version' utility function.

---
+++ bcftbx/__init__.py
@@ -0,0 +1,8 @@
+# Current version of the bcftbx package
+__version__ = '0.99.0'
+
+def get_version():
+    """Returns a string with the current version of the bcftbx package (e.g., "0.2.0")
+
+    """
+    return __version__
