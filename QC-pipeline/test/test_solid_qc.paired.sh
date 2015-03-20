e59b2f4108aacab5f85760949523c2e96d0be8b7
Peter Briggs <peter.briggs@manchester.ac.uk>
2015-03-20 10:31:02 +0000

    QC-pipeline/test: updated test scripts.

--- QC-pipeline/test/test_solid_qc.paired.sh
+++ QC-pipeline/test/test_solid_qc.paired.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
 #
-name=$(basename $0)
+name=$(basename ${0%.*})
 test_dir=$(mktemp --directory --tmpdir=. --suffix=.$name)
+data_files=\
+"solid_F3_sample3.csfasta \
+solid_F3_QV_sample3.qual \
+solid_F5-BC_sample3.csfasta \
+solid_F5-BC_QV_sample3.qual"
 echo Running tests in $test_dir
 cd $test_dir
-for f in ../*sample3*.csfasta ../*sample3*.qual ; do
-  ln -s $f
+for f in $data_files ; do
+  ln -s ../$f
 done
 run_qc_pipeline.py --runner=simple solid_qc.sh --input=solid_paired_end .
-qcreporter.py --platform=solid --verify .
+qcreporter.py --platform=solid --format=solid_paired_end --verify .
 retcode=$?
 echo FINISHED: $retcode
 exit $retcode
--- QC-pipeline/test/test_solid_qc.sh
+++ QC-pipeline/test/test_solid_qc.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 #
-test_dir=$(mktemp --directory --tmpdir=. --suffix=.test_solid_qc)
+name=$(basename ${0%.*})
+test_dir=$(mktemp --directory --tmpdir=. --suffix=.$name)
+data_files=\
+"solid_F3_sample1.csfasta \
+solid_F3_QV_sample1.qual \
+solid_F3_sample2.csfasta \
+solid_F3_QV_sample2.qual"
 echo Running tests in $test_dir
 cd $test_dir
-for f in ../*.csfasta ../*.qual ; do
-  ln -s $f
+for f in $data_files ; do
+  ln -s ../$f
 done
 run_qc_pipeline.py --runner=simple solid_qc.sh .
 qcreporter.py --platform=solid --verify .
