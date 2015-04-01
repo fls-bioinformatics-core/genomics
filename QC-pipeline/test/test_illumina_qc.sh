ccda92c0a9994679cc043d197cb3823f869664b2
Peter Briggs <peter.briggs@manchester.ac.uk>
2015-04-01 16:38:18 +0100

    QC-pipeline/test: added test data and script for Illumina data.

---
+++ QC-pipeline/test/test_illumina_qc.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#
+name=$(basename ${0%.*})
+test_dir=$(mktemp --directory --tmpdir=. --suffix=.$name)
+data_files=\
+"illumina_sample1_R1.fastq.gz \
+illumina_sample1_R2.fastq.gz"
+echo Running tests in $test_dir
+cd $test_dir
+for f in $data_files ; do
+  ln -s ../$f
+done
+run_qc_pipeline.py --input=fastqgz--runner=simple illumina_qc.sh .
+qcreporter.py --format=fastqgz --platform=illumina --verify .
+retcode=$?
+echo FINISHED: $retcode
+exit $retcode
+##
+#
