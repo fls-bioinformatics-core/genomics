96c0321cf4b65898674da64a7a749851e6df37b5
Peter Briggs <peter.briggs@manchester.ac.uk>
2015-04-02 16:31:33 +0100

    QC-pipeline/test: add test script for uncompress_fastqgz.sh.

---
+++ QC-pipeline/test/test_uncompress_fastqgz.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#
+name=$(basename ${0%.*})
+test_dir=$(mktemp --directory --tmpdir=. --suffix=.$name)
+echo Running tests in $test_dir
+cd $test_dir
+ln -s ../illumina_sample1_R1.fastq.gz
+uncompress_fastqgz.sh illumina_sample1_R1.fastq.gz
+retcode=$?
+echo FINISHED: $retcode
+exit $retcode
+##
+#
