5c54163a036361ea65f021b68e789e8ea1c34a54
Peter Briggs <peter.briggs@manchester.ac.uk>
2017-02-23 11:01:08 +0000

    Bump version to 1.0.0 and update ChangeLog.

--- bcftbx/__init__.py
+++ bcftbx/__init__.py
@@ -1,5 +1,5 @@
 # Current version of the bcftbx package
-__version__ = '0.99.15'
+__version__ = '1.0.0'
 
 def get_version():
     """Returns a string with the current version of the bcftbx package (e.g., "0.2.0")
