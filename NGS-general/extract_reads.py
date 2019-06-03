157ce463215c26770ff9f0cf3a570d9a8e1715c5
Peter Briggs <peter.briggs@manchester.ac.uk>
2019-06-03 16:24:17 +0100

    NGS-general: update extract_reads.py to use argparse instead of optparse.

--- NGS-general/extract_reads.py
+++ NGS-general/extract_reads.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #
 #     extract_reads.py: write random subsets of read records from input files
-#     Copyright (C) University of Manchester 2012 Peter Briggs
+#     Copyright (C) University of Manchester 2012,2019 Peter Briggs
 #
 ########################################################################
 #
@@ -29,7 +29,7 @@ Recognises FASTQ, CSFASTA and QUAL files.
 import sys
 import os
 import gzip
-import optparse
+import argparse
 import random
 import re
 from bcftbx.ngsutils import getreads
@@ -40,7 +40,7 @@ from bcftbx.ngsutils import getreads_regex
 # Module metadata
 #######################################################################
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 __description__ = """Extract subsets of reads from each of the
 supplied files according to specified criteria (e.g. random,
@@ -55,32 +55,31 @@ def main(args=None):
     # Command line processing
     if args is None:
         args = sys.argv[1:]
-    p = optparse.OptionParser(usage="%prog -m PATTERN |-n NREADS infile "
-                              "[ infile ... ]",
-                              version="%prog "+__version__,
-                              description=__description__)
-    p.add_option('-m','--match',action='store',dest='pattern',default=None,
-                 help="extract records that match Python regular "
-                 "expression PATTERN")
-    p.add_option('-n',action='store',dest='n',default=None,
-                 help="extract N random reads from the input file(s). "
-                 "If multiple files are supplied (e.g. R1/R2 pair) then "
-                 "the same subsets will be extracted for each. "
-                 "(Optionally a percentage can be supplied instead e.g. "
-                 "'50%' to extract a subset of half the reads.)")
-    p.add_option('-s','--seed',action='store',dest='seed',default=None,
-                 help="specify seed for random number generator (used "
-                 "for -n option; using the same seed should produce the "
-                 "same 'random' sample of reads)")
-    opts,args = p.parse_args(args)
-    if len(args) < 1:
-        p.error("Need to supply at least one input file")
+    p = argparse.ArgumentParser(description=__description__)
+    p.add_argument("--version",action='version',version=__version__)
+    p.add_argument('-m','--match',action='store',dest='pattern',
+                   default=None,
+                   help="extract records that match Python regular "
+                   "expression PATTERN")
+    p.add_argument('-n',action='store',dest='n',default=None,
+                   help="extract N random reads from the input file(s). "
+                   "If multiple files are supplied (e.g. R1/R2 pair) then "
+                   "the same subsets will be extracted for each. "
+                   "(Optionally a percentage can be supplied instead e.g. "
+                   "'50%%' to extract a subset of half the reads.)")
+    p.add_argument('-s','--seed',action='store',dest='seed',default=None,
+                   help="specify seed for random number generator (used "
+                   "for -n option; using the same seed should produce the "
+                   "same 'random' sample of reads)")
+    p.add_argument('infiles',metavar='infile',nargs='+',
+                   help="input FASTQ, CSFASTA, or QUAL file")
+    args = p.parse_args(args)
     # Pattern matching option
-    if opts.pattern is not None:
-        if opts.n is not None:
+    if args.pattern is not None:
+        if args.n is not None:
             p.error("Need to supply only one of -n or -m options")
-        print "Extracting reads matching '%s'" % opts.pattern
-        for f in args:
+        print "Extracting reads matching '%s'" % args.pattern
+        for f in args.infiles:
             if f.endswith('.gz'):
                 outfile = os.path.basename(os.path.splitext(f[:-3])[0])
             else:
@@ -88,27 +87,27 @@ def main(args=None):
             outfile += '.subset_regex.fq'
             print "Extracting to %s" % outfile
             with open(outfile,'w') as fp:
-                for read in getreads_regex(f,opts.pattern):
+                for read in getreads_regex(f,args.pattern):
                     fp.write('\n'.join(read) + '\n')
     else:
         # Seed random number generator
-        if opts.seed is not None:
-            random.seed(opts.seed)
+        if args.seed is not None:
+            random.seed(args.seed)
         # Count the reads
-        nreads = sum(1 for i in getreads(args[0]))
+        nreads = sum(1 for i in getreads(args.infiles[0]))
         print "Number of reads: %s" % nreads
-        if len(args) > 1:
+        if len(args.infiles) > 1:
             print "Verifying read numbers match between files"
-        for f in args[1:]:
+        for f in args.infiles[1:]:
             if sum(1 for i in getreads(f)) != nreads:
                 print "Inconsistent numbers of reads between files"
                 sys.exit(1)
         # Generate a subset of read indices to extract
         try:
-            nsubset = int(opts.n)
+            nsubset = int(args.n)
         except ValueError:
-            if str(opts.n).endswith('%'):
-                nsubset = int(float(opts.n[:-1])*nreads/100.0)
+            if str(args.n).endswith('%'):
+                nsubset = int(float(args.n[:-1])*nreads/100.0)
         if nsubset > nreads:
             print "Requested subset (%s) is larger than file (%s)" % (nsubset,
                                                                       nreads)
@@ -116,7 +115,7 @@ def main(args=None):
         print "Generating set of %s random indices" % nsubset
         subset_indices = random.sample(xrange(nreads),nsubset)
         # Extract the reads to separate files
-        for f in args:
+        for f in args.infiles:
             if f.endswith('.gz'):
                 outfile = os.path.basename(os.path.splitext(f[:-3])[0])
             else:
