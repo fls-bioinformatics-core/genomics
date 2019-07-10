4eaba207fd8bf6cb8c55957ff7620ea631f3a7cc
Peter Briggs <peter.briggs@manchester.ac.uk>
2019-07-10 12:43:25 +0100

    microarray/xrorthologs.py: update to treat strings as unicode, and drop --test option.

--- microarray/xrorthologs.py
+++ microarray/xrorthologs.py
@@ -44,6 +44,7 @@ appended based on matching up the probe set ids.
 
 import sys
 import os
+import io
 import logging
 import argparse
 
@@ -90,7 +91,7 @@ class ProbeSetLookup(object):
         self.__reverse_lookup = {}
         # Open file and read in data
         if lookup_data_fp is None:
-            fp = open(lookup_data_file,'rU')
+            fp = io.open(lookup_data_file,'rt')
         else:
             fp = lookup_data_fp
         for line in fp:
@@ -157,7 +158,7 @@ class IndexedFile(object):
         self.__keys = []
         self.__content = {}
         if fp is None:
-            fp = open(filen,'rU')
+            fp = io.open(filen,'rt')
             close_fp = True
         else:
             close_fp = False
@@ -229,7 +230,7 @@ def combine_data(data_file_1,data_file_2,lookup,outfile):
     data2 = IndexedFile(data_file_2,first_line_is_header=True)
 
     # Open output file
-    fp = open(outfile,'w')
+    fp = io.open(outfile,'wt')
 
     # Call main function to do the actual work
     combine_data_main(data1,data2,lookup,fp)
@@ -286,7 +287,6 @@ def combine_data_main(data1,data2,lookup,foutput):
 #######################################################################
 
 import unittest
-import cStringIO
 
 class TestProbeSetLookup(unittest.TestCase):
     """Test the ProbeSetLookup class
@@ -294,7 +294,7 @@ class TestProbeSetLookup(unittest.TestCase):
 
     def setUp(self):
         # Example look up data
-        self.fp1 = cStringIO.StringIO("""ps_1	gid_1	gid_2	ps_2
+        self.fp1 = io.StringIO(u"""ps_1	gid_1	gid_2	ps_2
 1053_at	5982	19718	1417503_at,1457638_x_at,1457669_x_at
 117_at	3310	NA	NA
 121_at	7849	18510	1418208_at,1446561_at
@@ -302,7 +302,7 @@ class TestProbeSetLookup(unittest.TestCase):
 1294_at	7318	74153	1426970_a_at,1426971_at,1437317_at
 203281_s_at	7318	74153	1426970_a_at,1426971_at,1437317_at
 """)
-        self.fp2 = cStringIO.StringIO("""ps_1	gid_1	gid_2	ps_2
+        self.fp2 = io.StringIO(u"""ps_1	gid_1	gid_2	ps_2
 1053_at	1417503_at,1457638_x_at,1457669_x_at
 117_at	NA
 121_at	1418208_at,1446561_at
@@ -347,7 +347,7 @@ class TestIndexedFile(unittest.TestCase):
     """
     def setUp(self):
         # Example data
-        self.fp = cStringIO.StringIO("""1007_s_at	U48705	chr6:30856165-30867931 (+) // 95.63 // p21.33	discoidin domain receptor tyrosine kinase 1	DDR1	900.8253051	1002.945639	0.898179	-1.11336
+        self.fp = io.StringIO(u"""1007_s_at	U48705	chr6:30856165-30867931 (+) // 95.63 // p21.33	discoidin domain receptor tyrosine kinase 1	DDR1	900.8253051	1002.945639	0.898179	-1.11336
 1053_at	M87338	chr7:73646002-73668732 (-) // 70.86 // q11.23	"replication factor C (activator 1) 2, 40kDa"	RFC2	953.0037594	1100.132756	0.866288	-1.15435
 117_at	X51757	chr1:161494448-161496380 (+) // 99.59 // q23.3 /// chr1:161576080-161578007 (+) // 98.03 // q23.3	heat shock 70kDa protein 6 (HSP70B')	HSPA6	37.34984439	43.5182996	0.858256	-1.16515
 121_at	X69699	chr2:113974939-114036488 (-) // 98.3 // q13	paired box 8	PAX8	293.4040238233.8182798	1.25484	1.25484
@@ -366,13 +366,13 @@ class TestIndexedFile(unittest.TestCase):
 class TestCombineData(unittest.TestCase):
     def setUp(self):
         # Example data
-        self.species1 = """Probe_Set_ID	Public_ID
+        self.species1 = u"""Probe_Set_ID	Public_ID
 1053_at	M87338
 117_at	X51757
 121_at	X69699
 1255_g_at	L36861
 """
-        self.species2 = """Probe_Set_ID	Public_ID
+        self.species2 = u"""Probe_Set_ID	Public_ID
 1417503_at	NM_020022
 1457638_x_at	AV039064
 1457669_x_at	AV096765
@@ -383,14 +383,14 @@ class TestCombineData(unittest.TestCase):
 1426971_at	AK004894
 1437317_at	BB735820
 """
-        self.lookup_data = """ps_1	gid_1	gid_2	ps_2
+        self.lookup_data = u"""ps_1	gid_1	gid_2	ps_2
 1053_at	5982	19718	1417503_at,1457638_x_at,1457669_x_at
 117_at	3310	NA	NA
 121_at	7849	18510	1418208_at,1446561_at
 1255_g_at	2978	14913	1421061_at
 1294_at	7318	74153	1426970_a_at,1426971_at,1437317_at
 """
-        self.expected_output = """Probe_Set_ID	Public_ID	Probe_Set_ID_1	Public_ID_1	Probe_Set_ID_2	Public_ID_2	Probe_Set_ID_3	Public_ID_3
+        self.expected_output = u"""Probe_Set_ID	Public_ID	Probe_Set_ID_1	Public_ID_1	Probe_Set_ID_2	Public_ID_2	Probe_Set_ID_3	Public_ID_3
 1053_at	M87338	1417503_at	NM_020022	1457638_x_at	AV039064	1457669_x_at	AV096765
 117_at	X51757
 121_at	X69699	1418208_at	NM_011040	1446561_at	BB497767
@@ -401,24 +401,15 @@ class TestCombineData(unittest.TestCase):
         """Test combine_data function
         """
         # Input data
-        data1 = IndexedFile(fp=cStringIO.StringIO(self.species1),first_line_is_header=True)
-        data2 = IndexedFile(fp=cStringIO.StringIO(self.species2),first_line_is_header=True)
-        lookup = ProbeSetLookup(lookup_data_fp=cStringIO.StringIO(self.lookup_data))
+        data1 = IndexedFile(fp=io.StringIO(self.species1),first_line_is_header=True)
+        data2 = IndexedFile(fp=io.StringIO(self.species2),first_line_is_header=True)
+        lookup = ProbeSetLookup(lookup_data_fp=io.StringIO(self.lookup_data))
         # Output "file"
-        output_fp = cStringIO.StringIO()
+        output_fp = io.StringIO()
         # Run data combination
         combine_data_main(data1,data2,lookup.lookup,output_fp)
         # Check output
         self.assertEqual(output_fp.getvalue(),self.expected_output)
-
-def run_tests():
-    print("Running unit tests")
-    suite = unittest.TestSuite(unittest.TestLoader().\
-                                   discover(os.path.dirname(sys.argv[0]), \
-                                                pattern=os.path.basename(sys.argv[0])))
-    unittest.TextTestRunner(verbosity=2).run(suite)
-    print("Tests finished")
-    sys.exit()
 
 #######################################################################
 # Main program
@@ -447,9 +438,6 @@ if __name__ == "__main__":
     p.add_argument("--debug",action="store_true",dest="debug",
                    default=False,
                    help="Turn on debugging output")
-    p.add_argument('--test',action="store_true",dest="run_tests",
-                   default=False,
-                   help="Run unit tests")
     p.add_argument('lookup',metavar="LOOKUPFILE",
                    help="tab-delimited file with one probe set "
                    "for species 1 per line in first column and a "
@@ -465,9 +453,6 @@ if __name__ == "__main__":
 
     # Debugging output
     if arguments.debug: logging.getLogger().setLevel(logging.DEBUG)
-
-    # Unit tests
-    if arguments.run_tests: run_tests()
 
     # Construct lookup object from data file
     print("Building lookup tables from %s" % arguments.lookup)
