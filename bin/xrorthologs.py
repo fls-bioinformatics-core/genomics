b8e66226b828785cbc9253521e50e79f8fc7b12d
Peter Briggs <peter.briggs@manchester.ac.uk>
2021-11-24 17:22:38 +0000

    microarray: refactor 'xrorthologs.py' to move into bcftbx.cli.

---
+++ bin/xrorthologs.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+#
+#     xrorthologs.py: cross-reference data for two species given probeset lookup
+#     Copyright (C) University of Manchester 2021 Peter Briggs
+#
+from bcftbx.cli.xrorthologs import main
+if __name__ == "__main__":
+     main()
