b036d03e32005d9f856b7e5aeed225f2997e80cf
Peter Briggs <peter.briggs@manchester.ac.uk>
2021-11-24 10:44:03 +0000

    Add 'napoleon' extension to the Sphinx documentation generation config.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,9 @@ sys.path.append('../')
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc']
+extensions = ['sphinx.ext.autodoc',
+              'sphinx.ext.napoleon',
+              'sphinx.ext.intersphinx',]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
