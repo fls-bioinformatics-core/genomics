3b2284ba586bd0f6f1b571c4554addd54dadb12a
Peter Briggs <peter.briggs@manchester.ac.uk>
2021-11-24 16:01:36 +0000

    RNA-seq: refactor the 'bowtie_mapping_stats.py' utility to put into bcftbx.cli.

---
+++ bin/bowtie_mapping_stats.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+#
+#     bowtie_mapping_stats.py: write Bowtie mapping stats to a spreadsheet
+#     Copyright (C) University of Manchester 2021 Peter Briggs
+#
+from bcftbx.cli.bowtie_mapping_stats import main
+if __name__ == "__main__":
+     main()
