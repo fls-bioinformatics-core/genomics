920a11b16fd901f82a08a1313d9ce4eeddc6a677
Peter Briggs <peter.briggs@manchester.ac.uk>
2021-11-24 14:13:44 +0000

    illumina2cluster/verify_paired.py: refactored into bcftbx.cli and moved into 'bin'.

---
+++ bin/verify_paired.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+#
+#     verify_paired.py: check R1 and R2 fastq files are consistent
+#     Copyright (C) University of Manchester 2021 Peter Briggs
+#
+from bcftbx.cli.verify_paired import main
+if __name__ == "__main__":
+     main()
