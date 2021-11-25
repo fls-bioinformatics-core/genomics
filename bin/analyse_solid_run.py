230ba774a2fdc6e6795d15dae582433ad0e9136d
Peter Briggs <peter.briggs@manchester.ac.uk>
2021-11-25 09:45:42 +0000

    solid2cluster/analyse_solid_run.py: relocate to bcftbx.cli.

---
+++ bin/analyse_solid_run.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+#
+#     analyse_solid_run.py: analyse and report on SOLiD sequencer runs
+#     Copyright (C) University of Manchester 2021 Peter Briggs
+#
+from bcftbx.cli.analyse_solid_run import main
+if __name__ == "__main__":
+     main()
