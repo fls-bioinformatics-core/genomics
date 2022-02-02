8c45bea1f27112eac388c5be38d91def7dc831cb
Peter Briggs <peter.briggs@manchester.ac.uk>
2022-02-02 15:05:36 +0000

    fastq_strand.py: add Python utility script to 'bin' (was missing from previous commits).

---
+++ bin/fastq_strand.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+#
+#     fastq_strand.py: determine strandedness of fastq pair using STAR
+#     Copyright (C) University of Manchester 2022 Peter Briggs
+#
+from bcftbx.cli.fastq_strand import main
+if __name__ == "__main__":
+     main()
