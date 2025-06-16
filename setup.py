a4cd2b5080507be1f3c5b476f9019b46c81295b5
Peter Briggs <peter.briggs@manchester.ac.uk>
2025-06-16 16:59:59 +0100

    setup.py: add 'bcftbx.platforms.illumina' to installation.

--- setup.py
+++ setup.py
@@ -28,7 +28,6 @@ setup(name = "genomics-bcftbx",
       packages = ['bcftbx',
                   'bcftbx.cli',
                   'bcftbx.platforms',
-                  'bcftbx.platforms.solid',
                   'bcftbx.qc'],
       license = 'AFL-3',
       # Pull in dependencies
